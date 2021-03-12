e40b8258dc5b37c5157ae9054d08d900176748b6
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-12 12:42:48 +0000

    moved subroutine output_acceptance_rates from output_xy_metropolis_acceptance_rates.f90 to xy_metropolis_algorithm.f90; deleted output_xy_metropolis_acceptance_rates.f90

--- src/xy/metropolis/xy_metropolis_algorithm.f90
+++ src/xy/metropolis/xy_metropolis_algorithm.f90
@@ -101,3 +101,31 @@ subroutine markov_chain_XY
   
   return
 end subroutine markov_chain_XY
+
+! **************************************
+! OUTPUT acceptance rates
+! **************************************
+
+subroutine output_acceptance_rates
+  use variables
+  implicit none
+  character(100) filename
+  real*8 acceptanceRate, twistAcceptanceRate
+
+  acceptanceRate = float(accept) / (measurements * volume)
+  twistAcceptanceRate = float(accept_twist) / (measurements * volume)
+
+  write (filename, '( "temp_eq_", F4.2,"//acceptance_rates_XY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' ) &
+       T, side, side, T
+  open(unit = 300, file = filename)
+
+  write(300, 100) acceptanceRate
+  if (twist .eq. 1) then
+     write(300, 100) twistAcceptanceRate
+  end if
+  close(300)
+
+100 format(F16.8)
+
+  return
+end subroutine output_acceptance_rates
