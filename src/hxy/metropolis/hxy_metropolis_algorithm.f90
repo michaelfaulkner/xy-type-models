0746c690aa2ab71ae71fca828e3a3220df0eb963
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-12 13:19:36 +0000

    restructured hxy source code, to mirror ecmc source code

--- src/hxy/metropolis/hxy_metropolis_algorithm.f90
+++ src/hxy/metropolis/hxy_metropolis_algorithm.f90
@@ -37,6 +37,8 @@ program metrop_HXY
         call markov_chain_HXY
      end do
 
+     accept = 0
+     accept_twist = 0                                                                ! SET TOTAL NUMBER OF GLOBAL TWISTS TO ZERO
      call initial_measure
      
      do j = 0, measurements - 1
@@ -134,3 +136,31 @@ subroutine markov_chain_HXY
   
   return
 end subroutine markov_chain_HXY
+
+! **************************************
+! OUTPUT acceptance rates
+! **************************************
+
+subroutine output_acceptance_rates
+  use variables
+  implicit none
+  character(100) filename
+  real*8 acceptanceRate, twistAcceptanceRate
+
+  acceptanceRate = float(accept) / (measurements * volume)
+  twistAcceptanceRate = float(accept_twist) / (measurements * volume)
+
+  write (filename, '( "temp_eq_", F4.2,"//acceptance_rates_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' ) &
+       T, side, side, T
+  open(unit = 300, file = filename)
+
+  write(300, 100) acceptanceRate
+  if (twist .eq. 1) then
+     write(300, 100) twistAcceptanceRate
+  end if
+  close(300)
+
+100 format(F16.8)
+
+  return
+end subroutine output_acceptance_rates
---
+++ src/hxy/metropolis/setup_hxy_metropolis_algorithm.f90
@@ -0,0 +1,54 @@
+! **************************************
+! SET VARIABLES
+! **************************************
+
+module variables
+  real*8, parameter :: twopi = 6.28318530718
+  real*8, parameter :: pi = 3.14159265359
+  real*8, parameter :: epsilon = 10.0 ** (-6)
+  integer max_side, max_sites
+  parameter (max_side = 128)
+  parameter (max_sites = max_side * max_side)
+  integer pos_x(max_sites), neg_x(max_sites), pos_y(max_sites), neg_y(max_sites), v(max_sites)
+  integer side, sites, Tsteps, thermSweeps, measurements, twist, accept, accept_twist, nmax
+  integer calculate_external_minimising_twist_field
+  integer  no_of_external_twists_to_minimise_potential_x, no_of_external_twists_to_minimise_potential_y
+  real*8  theta(max_sites), top_x(max_sites), top_y(max_sites)
+  real*8  sum_of_squared_electric_field_x, sum_of_squared_electric_field_y
+  real*8  volume, length, T, beta, Tmin, Tmax, proposalInterval, deltaProposalInterval
+end module variables
+
+
+! **************************************
+! READ IN INPUT HYPERPARAMETERS/CONSTANTS
+! **************************************
+
+subroutine input(seed, start)
+  use variables
+  implicit none
+  integer seed,start
+  
+  read(1,*) side
+  read(1,*) thermSweeps
+  read(1,*) measurements
+  read(1,*) Tmin
+  read(1,*) Tmax
+  read(1,*) Tsteps
+  read(1,*) proposalInterval
+  read(1,*) deltaProposalInterval
+  read(1,*) start
+  read(1,*) twist
+  read(1,*) nmax
+  read(1,*) seed
+
+  sites = side * side
+  volume = float(sites)
+  length = float(side)
+
+  if (side .gt. max_side) then
+     write(6,*) 'Linear lattice length exceeds maximum: change the maximum in the common file.'
+     stop
+  end if
+
+  return
+end subroutine input
