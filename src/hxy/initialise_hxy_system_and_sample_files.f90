d9fed16cdcbf36c286e393cac7aa61bf77dd9ce6
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-12 13:01:47 +0000

    created created initialise_hxy_system_and_sample_files.f90ported non-ecmc-specific setup subroutines from setup_hxy_ecmc_algorithm.f90 to initialise_hxy_system_and_sample_files.f90; exported non-ecmc-specific setup subroutines from setup_hxy_ecmc_algorithm.f90 to initialise_hxy_system_and_sample_files.f90

---
+++ src/hxy/initialise_hxy_system_and_sample_files.f90
@@ -0,0 +1,88 @@
+! **************************************
+! SETS NEIGHBOURS WITH PERIODIC BOUNDARY CONDITIONS
+! **************************************
+
+subroutine PBC
+  use variables
+  implicit none
+  integer i
+  ! mod(i + side + 1,side) RATHER THAN mod(i + 1,side), ETC. BELOW AS mod(x,side)
+  ! DOESN'T RETURN VALUES IN THE INTERVAL [0,side) FOR NEGATIVE x
+  do i = 0,sites - 1
+     pos_x(i) = i + mod(i + side + 1,side) - mod(i + side,side)
+     neg_x(i) = i + mod(i + side - 1,side) - mod(i + side,side)
+     pos_y(i) = i + (mod(int(i / side) + side + 1,side) - mod(int(i / side) + side,side)) * side
+     neg_y(i) = i + (mod(int(i / side) + side - 1,side) - mod(int(i / side) + side,side)) * side
+  end do
+  return
+end subroutine PBC
+
+! **************************************
+! INITIAL SPIN CONFIGURATION
+! **************************************
+
+subroutine initial_spins(start)
+  use variables
+  implicit none
+  integer start,i,j
+  if (start.eq.0) then
+     do i=0,sites-1
+        theta(i) = 0.0
+     end do
+  else
+     do i=0,sites-1
+        theta(i) = twopi * rand()
+     end do
+  end if
+  return
+end subroutine initial_spins
+
+! **************************************
+! SET CHAIN LENGTH AND Nevents TO 0; CREATE NEW DIRECTORY AND FILES FOR NEW TEMP
+! **************************************
+
+subroutine initial_measure
+  use variables
+  implicit none
+  character(100) filename
+  character(100) directory
+
+  write (directory, '( "temp_eq_", F4.2)' ) T                                     ! OPENS NEW DIRECTORY IN WHICH TO SAVE THE MARKOV CHAIN FOR THE CURRENT TEMPERATURE
+  call system ( 'mkdir -p ' // directory )
+
+  write (filename, '( "temp_eq_", F4.2,"//magn_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=10,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//magn_x_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=11,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//magn_y_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=12,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//cos_top_x_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=13,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//cos_top_y_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=14,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//sin_top_x_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=15,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//sin_top_y_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=16,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//Ebar_x_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=17,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//Ebar_y_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=18,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//vort_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' )  T,side,side,T
+  open(unit=19,file=filename)
+  write (filename, '( "temp_eq_", F4.2,"//second_deriv_potential_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' ) &
+       T,side,side,T
+  open(unit = 20, file = filename)
+  write (filename, '( "temp_eq_", F4.2,"//potential_samples_HXY_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' ) &
+       T,side,side,T
+  open(unit = 21, file = filename)
+
+  write (filename, '( "temp_eq_", F4.2,"//external_minimising_twist_field_x_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' ) &
+       T,side,side,T
+  open(unit = 22, file = filename)
+  write (filename, '( "temp_eq_", F4.2,"//external_minimising_twist_field_y_", I3.3, "x", I3.3, "_temp", F4.2, ".dat" )' ) &
+       T,side,side,T
+  open(unit = 23, file = filename)
+
+  return
+end subroutine initial_measure
