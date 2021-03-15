57d39304b75ce4eb741c5c1124cf1f6b48b56986
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-15 15:29:34 +0000

    pico reformatting

--- src/hxy/metropolis/setup_hxy_metropolis_algorithm.f90
+++ src/hxy/metropolis/setup_hxy_metropolis_algorithm.f90
@@ -3,20 +3,20 @@
 ! **************************************
 
 module variables
-  character(100) :: output_directory
-  real*8, parameter :: twopi = 6.28318530718
-  real*8, parameter :: pi = 3.14159265359
-  real*8, parameter :: epsilon = 10.0 ** (-6)
-  integer max_side, max_sites
-  parameter (max_side = 128)
-  parameter (max_sites = max_side * max_side)
-  integer pos_x(max_sites), neg_x(max_sites), pos_y(max_sites), neg_y(max_sites), v(max_sites)
-  integer side, sites, Tsteps, thermSweeps, measurements, twist, accept, accept_twist, nmax
-  integer calculate_external_minimising_twist_field
-  integer  no_of_external_twists_to_minimise_potential_x, no_of_external_twists_to_minimise_potential_y
-  real*8  theta(max_sites), top_x(max_sites), top_y(max_sites)
-  real*8  sum_of_squared_electric_field_x, sum_of_squared_electric_field_y
-  real*8  volume, length, T, beta, Tmin, Tmax, proposalInterval, deltaProposalInterval
+character(100) :: output_directory
+real*8, parameter :: twopi = 6.28318530718
+real*8, parameter :: pi = 3.14159265359
+real*8, parameter :: epsilon = 10.0 ** (-6)
+integer max_side, max_sites
+parameter (max_side = 128)
+parameter (max_sites = max_side * max_side)
+integer pos_x(max_sites), neg_x(max_sites), pos_y(max_sites), neg_y(max_sites), v(max_sites)
+integer side, sites, Tsteps, thermSweeps, measurements, twist, accept, accept_twist, nmax
+integer calculate_external_minimising_twist_field
+integer  no_of_external_twists_to_minimise_potential_x, no_of_external_twists_to_minimise_potential_y
+real*8  theta(max_sites), top_x(max_sites), top_y(max_sites)
+real*8  sum_of_squared_electric_field_x, sum_of_squared_electric_field_y
+real*8  volume, length, T, beta, Tmin, Tmax, proposalInterval, deltaProposalInterval
 end module variables
 
 
