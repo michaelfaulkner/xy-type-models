fb00bd42314dbf762aaa250ae4fb9140cd1ebed8
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-24 18:11:19 +0000

    mod() -> modulo() in initialise_xy_system_and_sample_files.f90

--- src/xy/initialise_xy_system_and_sample_files.f90
+++ src/xy/initialise_xy_system_and_sample_files.f90
@@ -5,10 +5,10 @@ integer :: i
 
 ! mod(i + side + 1, side) rather than mod(i + 1, side) below as mod(i, side) doesn't return value in [0, side) if i < 0
 do i = 1, sites
-    pos_x(i) = i + mod(i + side + 1, side) - mod(i + side, side)
-    neg_x(i) = i + mod(i + side - 1, side) - mod(i + side, side)
-    pos_y(i) = i + (mod(int(i / side) + side + 1, side) - mod(int(i / side) + side,side)) * side
-    neg_y(i) = i + (mod(int(i / side) + side - 1, side) - mod(int(i / side) + side,side)) * side
+    pos_x(i) = i + modulo(i + side + 1, side) - modulo(i + side, side)
+    neg_x(i) = i + modulo(i + side - 1, side) - modulo(i + side, side)
+    pos_y(i) = i + (modulo(int(i / side) + side + 1, side) - modulo(int(i / side) + side, side)) * side
+    neg_y(i) = i + (modulo(int(i / side) + side - 1, side) - modulo(int(i / side) + side, side)) * side
 end do
 
 return
