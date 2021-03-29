e7c08f746498a31e3b89139a9a7008efc4850862
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-29 14:37:54 +0100

    added d0 to end of floats in subroutine vortices (xy)

--- src/xy/xy_sampler.f90
+++ src/xy/xy_sampler.f90
@@ -99,9 +99,9 @@ integer :: i
 double precision :: v0
 do i = 1, sites
     v0 = (top_x(i) + top_y(i) - top_x(neg_x(i)) - top_y(neg_y(i))) / twopi
-    if ((v0 < 1.000000000001) .and. (v0 > 0.999999999999)) then
+    if ((v0 < 1.000000000001d0) .and. (v0 > 0.999999999999d0)) then
         v(i) = 1
-    else if ((v0 > -1.000000000001) .and. (v0 < -0.999999999999)) then
+    else if ((v0 > -1.000000000001d0) .and. (v0 < -0.999999999999d0)) then
         v(i) = -1
     else
         v(i) = 0
