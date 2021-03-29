e062ce1a64d28becffd9e9a8675f7fbbbe8a18b5
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-03-29 16:28:27 +0100

    integer, dimension (0:3) :: neighbours  ->  integer, dimension (1:4) :: neighbours

--- src/hxy/ecmc/hxy_ecmc_algorithm.f90
+++ src/hxy/ecmc/hxy_ecmc_algorithm.f90
@@ -60,30 +60,29 @@ subroutine event_chain_HXY
   use variables
   implicit none
   integer :: i, lift, veto
-  integer, dimension (0:3) :: neighbours
+  integer, dimension (1:4) :: neighbours
   double precision :: Estar,distanceToNextEvent,deltaEinitial,deltaEexit,distanceToGo
   double precision :: thetalift,thetafix,deltaThetaInitial,Ntours,deltaThetaExit,distance
-  
+
   lift = int(volume * rand())                                                       ! PICK A SITE AT RANDOM FOR INITIAL LIFTING VARIABLE (FROM {0, 1, ..., N-1})
   distanceToGo = maxchainlength                                                     ! SET REMAINING EVENT-CHAIN LENGTH AS THE MAX CHAIN LENGTH
-  
+
   do                                                                                ! ITERATE UNTIL THE EVENT-CHAIN LENGTH EXCEEDS THE MAXIMUM PERMITTED: THEN WE EXIT SUBROUTINE, MEASURE AND RESAMPLE THE LIFTING SPIN/PARAMETER
      thetalift = theta(lift)
-     
-     neighbours(0) = neg_x(lift)
-     neighbours(1) = pos_x(lift)
-     neighbours(2) = neg_y(lift)
-     neighbours(3) = pos_y(lift)
-     
-     distanceToNextEvent = 10d10                                                    ! RESET DISTANCE COUNTER FOR NEW SUB-CHAIN (WHERE SUB-CHAINS CONNECT EVENTS WITHIN AN EVENT CHAIN)
-     
-     do i = 0, 3                                                                    ! ITERATE OVER lift'S NEIGHBOURING SPINS/PARAMETERS
 
+     neighbours(1) = neg_x(lift)
+     neighbours(2) = pos_x(lift)
+     neighbours(3) = neg_y(lift)
+     neighbours(4) = pos_y(lift)
+
+     distanceToNextEvent = 10d10                                                    ! RESET DISTANCE COUNTER FOR NEW SUB-CHAIN (WHERE SUB-CHAINS CONNECT EVENTS WITHIN AN EVENT CHAIN)
+
+     do i = 1, 4                                                                    ! ITERATE OVER lift'S NEIGHBOURING SPINS/PARAMETERS
         thetafix = theta(neighbours(i))                                             ! THE VALUE OF EACH NEIGHBOURING SPIN/PARAMETER
         deltaThetaInitial = modulo(thetalift - thetafix + pi, twopi) - pi           ! CALCULATE SPIN/PARAMETER DIFFERENCE MODULO WRT (-PI,PI] (FORTRAN MOD FUNCTION FAILS FOR NEGATIVE VALUES HENCE THE IF LOOP)
         Estar = 1.0d0 - rand()                                                        ! DRAW UNIFORM CONT. RANDOM VARIABLE FROM (0,1] (TRANSFORM TO AVOIDS DIVERGENCES AT -ln(0))
         Estar = - T * log(Estar)
-        
+
         if (deltaThetaInitial > 0.0d0) then                                                     ! IF DERIVATIVE OF CONDITIONAL INT. POTENTIAL IS +VE
            deltaEinitial = 0.5d0 * deltaThetaInitial * deltaThetaInitial                        ! INITIAL CONDITIONAL INT. POTENTIAL
            Ntours = dble(int((deltaEinitial + Estar) / (twopi * twopi / 8.0d0)))                ! NO. COMPLETE ROTATIONS INDUCED THROUGH SPIN SPACE
@@ -96,14 +95,14 @@ subroutine event_chain_HXY
            deltaThetaExit = twopi / 2.0d0 - sqrt(twopi * twopi / 4.0d0 - 2.0d0 * deltaEexit)
            distance = (Ntours + 0.5d0) * twopi - (deltaThetaInitial + deltaThetaExit)
         end if
-        
+
         if (distanceToNextEvent > distance) then                                 ! IF LENGTH COVERED IS SHORTEST SO FAR
            distanceToNextEvent = distance
            veto = neighbours(i)                                                     ! UPDATE veto SPIN/PARAMETER
         end if
-        
+
      end do
-     
+
      if (distanceToNextEvent > distanceToGo) then                                  ! IF MAX. EVENT-CHAIN LENGTH HAS BEEN EXCEEDED
         theta(lift) = mod(thetalift + distanceToGo, twopi)              ! JUST ADD THE REMAINING LENGTH FROM TOTAL ALLOWED CHAIN LENGTH
         chainlength = chainlength + distanceToGo
