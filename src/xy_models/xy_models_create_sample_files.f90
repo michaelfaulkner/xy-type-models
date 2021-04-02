f71f651a4424b6541a6f555efbdd4facc3a1065e
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-02 15:56:03 +0100

    moved initialise_xy_system_and_sample_files.f90 up one directory and renamed as xy_models_create_sample_files.f90; created xy_models_initialise_spin_configuration.f90 and moved subroutine initialise_spin_configuration into this file; updated makefiles accordingly

--- src/xy_models/xy_models_create_sample_files.f90
+++ src/xy_models/xy_models_create_sample_files.f90
@@ -1,22 +1,3 @@
-subroutine initialise_spin_configuration(start)
-use variables
-implicit none
-integer :: i, start
-
-if (start == 0) then
-    do i = 1, sites
-        theta(i) = 0.0d0
-    end do
-else
-    do i = 1, sites
-        theta(i) = twopi * (rand() - 0.5d0)
-    end do
-end if
-
-return
-end subroutine initialise_spin_configuration
-
-
 subroutine create_sample_files
 use variables
 implicit none
---
+++ src/xy_models/xy_models_initialise_spin_configuration.f90
@@ -0,0 +1,17 @@
+subroutine initialise_spin_configuration(start)
+use variables
+implicit none
+integer :: i, start
+
+if (start == 0) then
+    do i = 1, sites
+        theta(i) = 0.0d0
+    end do
+else
+    do i = 1, sites
+        theta(i) = twopi * (rand() - 0.5d0)
+    end do
+end if
+
+return
+end subroutine initialise_spin_configuration
