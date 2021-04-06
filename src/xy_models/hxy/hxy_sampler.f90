8476cd3aee5f4dacbd0ca8cc8a20b8ef9d7be6c0
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-06 14:54:15 +0100

    subroutine vortices -> subroutine calculate_vortex_configuration and moved to calculate_vortex_configuration.f90

--- src/xy_models/hxy/hxy_sampler.f90
+++ src/xy_models/hxy/hxy_sampler.f90
@@ -77,29 +77,6 @@ end subroutine top_field
 
 
 ! **************************************
-! MEASURE NUMBER OF VORTICES
-! **************************************
-
-subroutine vortices
-use variables
-implicit none
-integer :: i
-double precision :: v0
-do i = 1, sites
-    v0 = (top_x(i) + top_y(i) - top_x(neg_x(i)) - top_y(neg_y(i))) / twopi
-    if ((v0 < 1.000000000001d0) .and. (v0 > 0.999999999999d0)) then
-        v(i) = 1
-    else if ((v0 > -1.000000000001d0) .and. (v0 < -0.999999999999d0)) then
-        v(i) = -1
-    else
-        v(i) = 0
-    end if
-end do
-return
-end subroutine vortices
-
-
-! **************************************
 ! Calculate the number of external twists required to minimise the current value of the potential
 ! **************************************
 
