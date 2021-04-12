7ea8e04b274bacf7fd082873930ceed8d803bebb
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-12 16:04:15 +0100

    fixed bugs in periodic_boundaries.f90 (which was introduced by switch from arrays starting from 1 instead of 0)

--- src/periodic_boundaries.f90
+++ src/periodic_boundaries.f90
@@ -5,10 +5,10 @@ integer :: i
 
 ! mod(i + side + 1, side) rather than mod(i + 1, side) below as mod(i, side) doesn't return value in [0, side) if i < 0
 do i = 1, sites
-    pos_x(i) = i + mod(i + side + 1, side) - mod(i + side, side)
-    neg_x(i) = i + mod(i + side - 1, side) - mod(i + side, side)
-    pos_y(i) = i + (mod(int(i / side) + side + 1, side) - mod(int(i / side) + side, side)) * side
-    neg_y(i) = i + (mod(int(i / side) + side - 1, side) - mod(int(i / side) + side, side)) * side
+    pos_x(i) = i + mod(i, side) - mod(i - 1, side)
+    neg_x(i) = i + mod(i - 2 + side, side) - mod(i - 1 + side, side)
+    pos_y(i) = i + (mod(int((i - 1) / side) + 1, side) - mod(int((i - 1) / side), side)) * side
+    neg_y(i) = i + (mod(int((i - 1) / side) + side - 1, side) - mod(int((i - 1) / side) + side, side)) * side
 end do
 
 return
