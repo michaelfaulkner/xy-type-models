816ee9ecf5af50e685f48e44ad404257a4fa1fe8
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-12 12:42:05 +0100

    maggs algos: twopi, pi and elementary_charge now all 15sf double precision values

--- src/maggs_electrolyte/setup_maggs_electrolyte_algorithm.f90
+++ src/maggs_electrolyte/setup_maggs_electrolyte_algorithm.f90
@@ -4,8 +4,8 @@
 
 module variables
 character(100) :: output_directory, algorithm_name
-double precision, parameter :: twopi = 6.28318531
-double precision, parameter :: pi = 3.14159265
+double precision, parameter :: twopi = 6.28318530717959d0
+double precision, parameter :: pi = 3.14159265358979d0
 double precision, parameter :: epsilon = 10.0 ** (-6)
 integer max_side, max_sites
 parameter (max_side = 128)
