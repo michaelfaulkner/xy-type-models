ec77159df0bb3faa368e4089389ee3060257e415
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-13 12:20:15 +0100

    xy metrop algo: pos_x, pos_y, neg_x, neg_y, array_of_sites and theta now allocatable arrays; removed top_x, top_y, pi, max_side, max_sites

--- src/xy_models/xy/metropolis/setup_xy_metropolis_algorithm.f90
+++ src/xy_models/xy/metropolis/setup_xy_metropolis_algorithm.f90
@@ -1,15 +1,12 @@
 module variables
-character(100) :: output_directory, algorithm_name
-integer, parameter :: max_side = 128
-integer, parameter :: max_sites = max_side * max_side
-double precision, parameter :: twopi = 6.28318530717959d0
-double precision, parameter :: pi = 3.14159265358979d0
-integer :: pos_x(max_sites), neg_x(max_sites), pos_y(max_sites), neg_y(max_sites), array_of_sites(max_sites)
+integer, allocatable, dimension(:) :: pos_x, neg_x, pos_y, neg_y, array_of_sites
 integer :: side, sites, no_of_temperature_increments, therm_sweeps, measurements, twist, no_of_accepted_local_moves
-integer :: no_of_accepted_external_global_moves
-double precision :: theta(max_sites), top_x(max_sites), top_y(max_sites)
+integer :: no_of_accepted_external_global_moves, no_of_events
+double precision, parameter :: twopi = 6.28318530717959d0
+double precision, allocatable, dimension(:) :: theta
 double precision :: volume, length, beta, temperature, initial_temperature, final_temperature
 double precision :: width_of_proposal_interval, magnitude_of_proposal_interval_increments
+character(100) :: output_directory, algorithm_name
 end module variables
 
 ! **************************************
@@ -43,14 +40,11 @@ temperature = initial_temperature
 sites = side * side
 volume = float(sites)
 
+allocate(theta(sites), pos_x(sites), pos_y(sites), neg_x(sites), neg_y(sites), array_of_sites(sites))
+
 do i = 1, sites
     array_of_sites(i) = i
 end do
-
-if (side > max_side) then
-   write(6,*) 'Linear lattice length exceeds maximum: change the maximum in module variables.'
-   stop
-end if
 
 return
 end subroutine input
