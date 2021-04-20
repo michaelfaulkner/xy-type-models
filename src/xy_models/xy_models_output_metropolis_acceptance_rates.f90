259d75be6293846471f0dad2d3ad45a2bf97aa18
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-20 12:15:56 +0100

    slight reformatting to metrop acceptance rates fortran files

--- src/xy_models/xy_models_output_metropolis_acceptance_rates.f90
+++ src/xy_models/xy_models_output_metropolis_acceptance_rates.f90
@@ -9,14 +9,14 @@ acceptance_rate_of_field_rotations = float(no_of_accepted_field_rotations) / (me
 acceptance_rate_of_external_global_moves = float(no_of_accepted_external_global_moves) / (measurements * volume)
 
 write(filename, '(A, F4.2, "//acceptance_rates.csv")') trim(output_directory)//trim(temperature_string), temperature
-open(unit=300, file = filename)
+open(unit=20, file = filename)
 
 if (twist /= 1) then
-    write(300, 100) acceptance_rate_of_field_rotations
+    write(20, 100) acceptance_rate_of_field_rotations
 else
-    write(300, 200) acceptance_rate_of_field_rotations, acceptance_rate_of_external_global_moves
+    write(20, 200) acceptance_rate_of_field_rotations, acceptance_rate_of_external_global_moves
 end if
-close(300)
+close(20)
 
 100 format(ES24.14)
 200 format(ES24.14, ", ", ES24.14)
