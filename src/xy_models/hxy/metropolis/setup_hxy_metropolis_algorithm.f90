9de98e806dc79875a8931b6dd7ee2b41e60d6ca7
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-22 13:56:33 +0100

    tidied setup_xy_metropolis_algorithm.f90 and setup_hxy_metropolis_algorithm.f90 (also introduced allocatable arrays in the latter)

--- src/xy_models/hxy/metropolis/setup_hxy_metropolis_algorithm.f90
+++ src/xy_models/hxy/metropolis/setup_hxy_metropolis_algorithm.f90
@@ -1,20 +1,17 @@
 module variables
 character(100) :: output_directory, algorithm_name
 logical :: use_external_global_moves, randomise_initial_field_configuration, calculate_external_minimising_twist_field
-integer, parameter :: max_integer_lattice_length = 128
-integer, parameter :: max_no_of_sites = max_integer_lattice_length * max_integer_lattice_length
-double precision, parameter :: twopi = 6.28318530717959d0
-double precision, parameter :: pi = 3.14159265358979d0
-integer :: pos_x(max_no_of_sites), neg_x(max_no_of_sites), pos_y(max_no_of_sites), neg_y(max_no_of_sites)
-integer :: array_of_sites(max_no_of_sites)
+integer, allocatable, dimension(:) :: pos_x, neg_x, pos_y, neg_y, array_of_sites
 integer :: integer_lattice_length, no_of_sites, no_of_temperature_increments, no_of_equilibration_sweeps
 integer :: no_of_observations, no_of_accepted_field_rotations, no_of_accepted_external_global_moves
 integer :: no_of_external_twists_to_minimise_potential_x, no_of_external_twists_to_minimise_potential_y
 integer :: vacuum_permittivity_sum_cutoff
-double precision :: theta(max_no_of_sites), emergent_field_x(max_no_of_sites), emergent_field_y(max_no_of_sites)
-double precision :: sum_of_squared_electric_field_x, sum_of_squared_electric_field_y
+double precision, parameter :: twopi = 6.28318530717959d0
+double precision, parameter :: pi = 3.14159265358979d0
+double precision, allocatable, dimension(:) :: theta, emergent_field_x, emergent_field_y
 double precision :: beta, temperature, initial_temperature, final_temperature, magnitude_of_temperature_increments
 double precision :: width_of_proposal_interval, target_acceptance_rate_of_field_rotations
+double precision :: sum_of_squared_electric_field_x, sum_of_squared_electric_field_y
 end module variables
 
 
@@ -44,14 +41,11 @@ if (algorithm_name /= 'hxy-metropolis') then
 end if
 
 no_of_sites = integer_lattice_length * integer_lattice_length
+allocate(theta(no_of_sites), emergent_field_x(no_of_sites), emergent_field_y(no_of_sites))
+allocate(pos_x(no_of_sites), pos_y(no_of_sites), neg_x(no_of_sites), neg_y(no_of_sites), array_of_sites(no_of_sites))
 do i = 1, no_of_sites
     array_of_sites(i) = i
 end do
-
-if (integer_lattice_length > max_integer_lattice_length) then
-    write(6, *) 'Linear lattice length exceeds maximum: change the maximum in the common file.'
-    stop
-end if
 
 return
 end subroutine read_in_config_file
--- src/xy_models/xy/metropolis/setup_xy_metropolis_algorithm.f90
+++ src/xy_models/xy/metropolis/setup_xy_metropolis_algorithm.f90
@@ -1,4 +1,6 @@
 module variables
+character(100) :: output_directory, algorithm_name
+logical :: use_external_global_moves, randomise_initial_field_configuration
 integer, allocatable, dimension(:) :: pos_x, neg_x, pos_y, neg_y, array_of_sites
 integer :: integer_lattice_length, no_of_sites, no_of_temperature_increments, no_of_equilibration_sweeps
 integer :: no_of_observations, no_of_accepted_field_rotations, no_of_accepted_external_global_moves
@@ -6,8 +8,6 @@ double precision, parameter :: twopi = 6.28318530717959d0
 double precision, allocatable, dimension(:) :: theta
 double precision :: beta, temperature, initial_temperature, final_temperature, magnitude_of_temperature_increments
 double precision :: width_of_proposal_interval, target_acceptance_rate_of_field_rotations
-character(100) :: output_directory, algorithm_name
-logical :: use_external_global_moves, randomise_initial_field_configuration
 end module variables
 
 
