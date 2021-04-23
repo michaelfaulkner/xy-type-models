06323932e483a6cd537f3591332e10be3d9ef892
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-23 13:04:45 +0100

    xy_attempt_external_global_move.f90: tidied and reconfigured to account for new lattice-site numbering system

--- src/xy_models/xy/xy_attempt_external_global_move.f90
+++ src/xy_models/xy/xy_attempt_external_global_move.f90
@@ -1,53 +1,61 @@
 subroutine attempt_external_global_move
 use variables
 implicit none
-integer :: i, rand1, rand2
-double precision :: deltaE
+integer :: i, lattice_site, initial_lattice_site
+double precision :: potential_difference, sign_of_twist
 
-deltaE = 0
-
-if (floor(2 * rand()) == 0) then                                                 ! X TWIST
-
- rand1 = floor(2 * rand())                                                       ! CHOOSES +VE OR -VE TWIST DIRECTION
- if (rand1 == 0) then
-    rand1 = -1
- end if
-
- do i = 1, no_of_sites
-    deltaE = deltaE - cos(spin_field(i) - spin_field(get_west_neighbour(i)) + rand1 * twopi / integer_lattice_length) &
-                    + cos(spin_field(i) - spin_field(get_west_neighbour(i)))
- end do
-
- if ((deltaE < 0) .or. (rand() < exp(-beta * deltaE))) then
-    rand2 = int(integer_lattice_length * rand())                                               ! TO PICK A RANDOM STARTING LINE FROM WHICH TO TWIST
+potential_difference = 0.0d0
+! choose a twist in x direction with 0.5 probability
+if (floor(2 * rand()) == 0) then
+    sign_of_twist = floor(2.0d0 * rand())
+    if (sign_of_twist < 0.5d0) then
+        sign_of_twist = -1.0d0
+    end if
+    ! pick initial lattice site from which to twist
+    initial_lattice_site = int(dfloat(integer_lattice_length) * rand())
+    lattice_site = initial_lattice_site
     do i = 1, no_of_sites
-       spin_field(i) = mod(spin_field(i) + rand1 * twopi * mod(i + rand2, integer_lattice_length) / &
-                            integer_lattice_length, twopi)
+        potential_difference = potential_difference - cos(spin_field(get_east_neighbour(lattice_site)) + sign_of_twist &
+                                                          * twopi / integer_lattice_length - spin_field(lattice_site)) &
+                                                    + cos(spin_field(get_east_neighbour(lattice_site)) &
+                                                          - spin_field(lattice_site))
+        lattice_site = mod(lattice_site + 1, no_of_sites)
     end do
-    no_of_accepted_external_global_moves = no_of_accepted_external_global_moves + 1
- end if
-
-else                                                                               ! Y TWIST
-
- rand1 = floor(2 * rand())                                                       ! CHOOSES +VE OR -VE TWIST DIRECTION
- if (rand1 == 0) then
-    rand1 = -1
- end if
-
- do i = 1, no_of_sites
-    deltaE = deltaE - cos(spin_field(i) - spin_field(get_south_neighbour(i)) + rand1 * twopi / integer_lattice_length) &
-                    + cos(spin_field(i) - spin_field(get_south_neighbour(i)))
- end do
-
- if ((deltaE < 0) .or. (rand() < exp(-beta * deltaE))) then
-    rand2 = int(integer_lattice_length * rand())                                                   ! TO PICK A RANDOM STARTING LINE FROM WHICH TO TWIST
+    if ((potential_difference < 0.0d0) .or. (rand() < exp(-beta * potential_difference))) then
+        lattice_site = initial_lattice_site
+        do i = 1, no_of_sites
+            spin_field(get_east_neighbour(lattice_site)) = mod(spin_field(get_east_neighbour(lattice_site)) &
+                                                                + sign_of_twist * twopi / integer_lattice_length, twopi)
+            lattice_site = mod(lattice_site + 1, no_of_sites)
+        end do
+        no_of_accepted_external_global_moves = no_of_accepted_external_global_moves + 1
+    end if
+! else: attempt a twist in y direction
+else
+    sign_of_twist = floor(2.0d0 * rand())
+    if (sign_of_twist < 0.5d0) then
+        sign_of_twist = -1.0d0
+    end if
+    ! pick initial lattice site from which to twist
+    initial_lattice_site = int(dfloat(integer_lattice_length) * rand())
+    lattice_site = initial_lattice_site
     do i = 1, no_of_sites
-       spin_field(i) = mod(spin_field(i) + rand1 * twopi * (int(i / integer_lattice_length) + rand2) / &
-                            integer_lattice_length, twopi)
+        potential_difference = potential_difference - cos(spin_field(get_north_neighbour(lattice_site)) + sign_of_twist &
+                                                          * twopi / integer_lattice_length - spin_field(lattice_site)) &
+                                                    + cos(spin_field(get_north_neighbour(lattice_site)) &
+                                                          - spin_field(lattice_site))
+        lattice_site = mod(lattice_site + 1, no_of_sites)
     end do
-    no_of_accepted_external_global_moves = no_of_accepted_external_global_moves + 1
- end if
-
+    if ((potential_difference < 0.0d0) .or. (rand() < exp(-beta * potential_difference))) then
+        lattice_site = initial_lattice_site
+        do i = 1, no_of_sites
+            spin_field(get_north_neighbour(lattice_site)) = mod(spin_field(get_north_neighbour(lattice_site)) &
+                                                                + sign_of_twist * twopi / integer_lattice_length, twopi)
+            lattice_site = mod(lattice_site + 1, no_of_sites)
+        end do
+        no_of_accepted_external_global_moves = no_of_accepted_external_global_moves + 1
+    end if
 end if
 
+return
 end subroutine attempt_external_global_move
