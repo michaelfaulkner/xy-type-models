c0ffd20c1001e2de399726fcd244ea9d840a0e72
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-24 16:58:42 +0100

    xy models src: split xy_models_initialise_field_configuration.f90 into xy_initialise_field_configuration.f90 and hxy_initialise_field_configuration.f90 (as the latter now contains call calculate_emergent_field)

---
+++ src/xy_models/hxy/hxy_initialise_field_configuration.f90
@@ -0,0 +1,18 @@
+subroutine initialise_field_configuration
+use variables
+implicit none
+integer :: i
+
+if (randomise_initial_field_configuration) then
+    do i = 1, no_of_sites
+        spin_field(i) = twopi * rand()
+    end do
+else
+    do i = 1, no_of_sites
+        spin_field(i) = 0.0d0
+    end do
+end if
+call calculate_emergent_field
+
+return
+end subroutine initialise_field_configuration
