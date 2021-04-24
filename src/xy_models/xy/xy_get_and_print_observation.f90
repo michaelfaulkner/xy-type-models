1b9ee2e27bbc63bdf37dc77d54c5d08375dafb74
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-24 17:33:29 +0100

    xy_get_and_print_observation.f90: magnetisation and other internal quantities switched to 2d arrays (from mag_x, mag_y)

--- src/xy_models/xy/xy_get_and_print_observation.f90
+++ src/xy_models/xy/xy_get_and_print_observation.f90
@@ -2,36 +2,32 @@ subroutine get_and_print_observation
 use variables
 implicit none
 integer :: i
-double precision :: magnetisation_x, magnetisation_y, potential
-double precision :: sum_of_1st_derivative_of_potential_x, sum_of_1st_derivative_of_potential_y
-double precision :: sum_of_2nd_derivative_of_potential_x, sum_of_2nd_derivative_of_potential_y
+double precision :: potential
+double precision :: magnetisation(2), sum_of_1st_derivative_of_potential(2), sum_of_2nd_derivative_of_potential(2)
 
-magnetisation_x = 0.0d0
-magnetisation_y = 0.0d0
-sum_of_1st_derivative_of_potential_x = 0.0d0
-sum_of_1st_derivative_of_potential_y = 0.0d0
-sum_of_2nd_derivative_of_potential_x = 0.0d0
-sum_of_2nd_derivative_of_potential_y = 0.0d0
 potential = 0.0d0
+magnetisation = (/ 0.0d0, 0.0d0 /)
+sum_of_1st_derivative_of_potential = (/ 0.0d0, 0.0d0 /)
+sum_of_2nd_derivative_of_potential = (/ 0.0d0, 0.0d0 /)
 
 do i = 1, no_of_sites
-    magnetisation_x = magnetisation_x + cos(spin_field(i))
-    magnetisation_y = magnetisation_y + sin(spin_field(i))
-    sum_of_1st_derivative_of_potential_x = sum_of_1st_derivative_of_potential_x &
+    magnetisation(1) = magnetisation(1) + cos(spin_field(i))
+    magnetisation(2) = magnetisation(2) + sin(spin_field(i))
+    sum_of_1st_derivative_of_potential(1) = sum_of_1st_derivative_of_potential(1) &
                                             + sin(spin_field(get_east_neighbour(i)) - spin_field(i))
-    sum_of_1st_derivative_of_potential_y = sum_of_1st_derivative_of_potential_y &
+    sum_of_1st_derivative_of_potential(2) = sum_of_1st_derivative_of_potential(2) &
                                             + sin(spin_field(get_north_neighbour(i)) - spin_field(i))
-    sum_of_2nd_derivative_of_potential_x = sum_of_2nd_derivative_of_potential_x &
+    sum_of_2nd_derivative_of_potential(1) = sum_of_2nd_derivative_of_potential(1) &
                                             + cos(spin_field(get_east_neighbour(i)) - spin_field(i))
-    sum_of_2nd_derivative_of_potential_y = sum_of_2nd_derivative_of_potential_y &
+    sum_of_2nd_derivative_of_potential(2) = sum_of_2nd_derivative_of_potential(2) &
                                             + cos(spin_field(get_north_neighbour(i)) - spin_field(i))
     potential = potential - cos(spin_field(get_east_neighbour(i)) - spin_field(i)) &
                           - cos(spin_field(get_north_neighbour(i)) - spin_field(i))
 end do
 
-write(20, 100) potential, magnetisation_x, magnetisation_y, &
-                sum_of_1st_derivative_of_potential_x, sum_of_1st_derivative_of_potential_y, &
-                sum_of_2nd_derivative_of_potential_x, sum_of_2nd_derivative_of_potential_y
+write(20, 100) potential, magnetisation(1), magnetisation(2), &
+                sum_of_1st_derivative_of_potential(1), sum_of_1st_derivative_of_potential(2), &
+                sum_of_2nd_derivative_of_potential(1), sum_of_2nd_derivative_of_potential(2)
 
 100 format(ES30.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14)
 
