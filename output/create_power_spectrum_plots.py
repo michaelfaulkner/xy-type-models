27db99ae50fa2c030c2351f13e04e2bc0c071bed
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-04-30 13:33:03 +0100

    first attempt at the power spectrum

---
+++ output/create_power_spectrum_plots.py
@@ -0,0 +1,74 @@
+import importlib
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+
+
+# Add the directory that contains config_file and markov_chain_diagnostics to sys.path
+this_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, this_directory)
+config_file = importlib.import_module('config_file')
+sample_getter = importlib.import_module('sample_getter')
+markov_chain_diagnostics = importlib.import_module('markov_chain_diagnostics')
+polyspectra = importlib.import_module('polyspectra')
+
+
+def main(config_file_name, power_spectrum_string):
+    basic_configuration_data = config_file.get_basic_configuration_data(config_file_name)
+    (algorithm_name, simulation_directory, lattice_length, number_of_equilibrium_iterations, initial_temperature,
+     final_temperature, number_of_temperature_increments) = (basic_configuration_data[0], basic_configuration_data[1],
+                                                             basic_configuration_data[2], basic_configuration_data[3],
+                                                             basic_configuration_data[5], basic_configuration_data[6],
+                                                             basic_configuration_data[7])
+
+    if ((algorithm_name == 'elementary-electrolyte' or algorithm_name == 'multivalued-electrolyte') and
+            (power_spectrum_string == 'magnetisation_norm' or power_spectrum_string == 'helicity_modulus' or
+             power_spectrum_string == 'inverse_vacuum_permittivity' or power_spectrum_string ==
+             'toroidal_vortex_polarisation')):
+        print('ConfigurationError: This is an Maggs-electrolyte model: do not give either magnetisation, '
+              'helicity_modulus, inverse_vacuum_permittivity or toroidal_vortex_polarisation as the second positional '
+              'argument.')
+        exit()
+    if ((algorithm_name == 'xy-ecmc' or algorithm_name == 'hxy-ecmc' or algorithm_name == 'xy-metropolis' or
+         algorithm_name == 'hxy-metropolis') and (power_spectrum_string == 'inverse_permittivity' or
+                                                  power_spectrum_string == 'topological_sector_fluctuations' or
+                                                  power_spectrum_string == 'toroidal_polarisation')):
+        print('ConfigurationError: This is an XY or HXY model: do not give either inverse_permittivity, '
+              'topological_sector_fluctuations or toroidal_polarisation as the second positional argument.')
+        exit()
+
+    if number_of_temperature_increments == 0:
+        magnitude_of_temperature_increments = 0.0
+    else:
+        magnitude_of_temperature_increments = (final_temperature -
+                                               initial_temperature) / number_of_temperature_increments
+    number_of_sites = lattice_length ** 2
+
+    matplotlib.rcParams['text.latex.preamble'] = r"\usepackage{amsmath}"
+    plt.xlabel(r"frequency, $\omega$", fontsize=15, labelpad=10)
+    plt.ylabel(r"$ S_X \left( \omega \right)$", fontsize=15, labelpad=10)
+    plt.tick_params(axis='both', which='major', labelsize=14, pad=10)
+
+    temperature = final_temperature
+    get_sample_method = getattr(sample_getter, 'get_' + power_spectrum_string)
+    colors = iter(plt.cm.rainbow(np.linspace(0, 1, number_of_temperature_increments + 1)))
+    for i in range(number_of_temperature_increments + 1):
+        beta = 1.0 / temperature
+        temperature_directory = '/temp_eq_' + f'{temperature:.2f}'
+        sample = get_sample_method(simulation_directory, temperature_directory, beta, number_of_sites)[
+                 number_of_equilibrium_iterations:]
+        power_spectrum = polyspectra.get_power_spectrum(sample, simulation_directory, temperature_directory)
+        plt.plot(power_spectrum[0][0:250], power_spectrum[1][0:250], color=next(colors),
+                 label=f'temperature = {temperature:.2f}')
+        plt.tight_layout()
+        temperature -= magnitude_of_temperature_increments
+    legend = plt.legend(loc='upper right', fontsize=10)
+    legend.get_frame().set_edgecolor('k')
+    legend.get_frame().set_lw(1.5)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
---
+++ output/polyspectra.py
@@ -0,0 +1,26 @@
+from scipy import signal
+import importlib
+import numpy as np
+import os
+import sys
+
+
+# Add the directory that contains config_file and markov_chain_diagnostics to sys.path
+this_directory = os.path.dirname(os.path.abspath(__file__))
+output_directory = os.path.abspath(this_directory + '/../')
+sys.path.insert(0, output_directory)
+sample_getter = importlib.import_module('sample_getter')
+
+
+def get_power_spectrum(sample, simulation_directory, temperature_directory, sampling_frequency=None):
+    if sampling_frequency is None:
+        acceptance_rates = sample_getter.get_acceptance_rates(simulation_directory, temperature_directory)
+        physical_time_scale = acceptance_rates[1] * acceptance_rates[0] ** 2 / 24.0
+        sampling_frequency = 1.0 / physical_time_scale
+    sample = np.atleast_2d(sample)
+    if len(sample) > 1:
+        sample = sample.transpose()
+    power_spectrum = np.atleast_2d([signal.periodogram(component, fs=sampling_frequency) for component in sample])
+    component_averaged_power_spectrum = np.mean(power_spectrum, axis=0)
+    return component_averaged_power_spectrum[0], np.array([item if abs(item) > 1.0e-15 else 0.0 for item in
+                                                           component_averaged_power_spectrum[1]])
