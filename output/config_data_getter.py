6789184cb6f7bc86a52ea2b23126bc36cd410484
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-05-06 14:02:50 +0100

    config_data_getter.py: integrated: if '' in str

--- output/config_data_getter.py
+++ output/config_data_getter.py
@@ -3,27 +3,25 @@ import csv
 
 def get_basic_data(config_file_name):
     with open(config_file_name, 'r') as config_file:
-        config_data = csv.reader(config_file, delimiter='\t')
-        for index, row in enumerate(config_data):
-            if index == 0:
+        for row in csv.reader(config_file, delimiter='\t'):
+            if 'algorithm_name' in row[0]:
                 algorithm_name = row[0].replace("'", "").replace("algorithm_name", "").replace(" ", "")
-            if index == 1:
-                simulation_directory = row[0].replace("'", "").replace("output directory", "").replace(" ", "")
-            if index == 2:
-                lattice_length = int(row[0].replace("'", "").replace("lattice length", "").replace(" ", ""))
-            if index == 3:
-                no_of_equilibrium_iterations = int(row[0].replace("number of equilibration iterations", "").replace(" ",
+            if 'output_directory' in row[0]:
+                output_directory = row[0].replace("'", "").replace("output_directory", "").replace(" ", "")
+            if 'integer_lattice_length' in row[0]:
+                integer_lattice_length = int(row[0].replace("'", "").replace("integer_lattice_length", "").replace(" ", ""))
+            if 'no_of_equilibration_sweeps' in row[0]:
+                no_of_equilibrium_iterations = int(row[0].replace("no_of_equilibration_sweeps", "").replace(" ",
                                                                                                                     ""))
-            if index == 4:
-                no_of_observations = int(row[0].replace("number of observations (sample size)", "").replace(" ", ""))
-            if index == 5:
-                initial_temperature = float(row[0].replace("d0", "").replace("initial temperature", "").replace(" ",
+            if 'no_of_observations' in row[0]:
+                no_of_observations = int(row[0].replace("no_of_observations", "").replace(" ", ""))
+            if 'initial_temperature' in row[0]:
+                initial_temperature = float(row[0].replace("d0", "").replace("initial_temperature", "").replace(" ",
                                                                                                                 ""))
-            if index == 6:
-                final_temperature = float(row[0].replace("d0", "").replace("final temperature", "").replace(" ", ""))
-            if index == 7:
-                no_of_temperature_increments = int(row[0].replace("number of temperature increments", "").replace(" ",
+            if 'final_temperature' in row[0]:
+                final_temperature = float(row[0].replace("d0", "").replace("final_temperature", "").replace(" ", ""))
+            if 'no_of_temperature_increments' in row[0]:
+                no_of_temperature_increments = int(row[0].replace("no_of_temperature_increments", "").replace(" ",
                                                                                                                   ""))
-                break
-    return (algorithm_name, simulation_directory, lattice_length, no_of_equilibrium_iterations, no_of_observations,
+    return (algorithm_name, output_directory, integer_lattice_length, no_of_equilibrium_iterations, no_of_observations,
             initial_temperature, final_temperature, no_of_temperature_increments)
