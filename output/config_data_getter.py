995c7a7ef80ff2da373f6538b359faef96156087
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-05-06 15:03:47 +0100

    config_data_getter.py: PEP8

--- output/config_data_getter.py
+++ output/config_data_getter.py
@@ -9,19 +9,17 @@ def get_basic_data(config_file_name):
             if 'output_directory' in row[0]:
                 output_directory = row[0].replace("'", "").replace("output_directory", "").replace(" ", "")
             if 'integer_lattice_length' in row[0]:
-                integer_lattice_length = int(row[0].replace("'", "").replace("integer_lattice_length", "").replace(" ", ""))
+                integer_lattice_length = int(row[0].replace("'", "").replace("integer_lattice_length", "").replace(" ",
+                                                                                                                   ""))
             if 'no_of_equilibration_sweeps' in row[0]:
-                no_of_equilibrium_iterations = int(row[0].replace("no_of_equilibration_sweeps", "").replace(" ",
-                                                                                                                    ""))
+                no_of_equilibrium_iterations = int(row[0].replace("no_of_equilibration_sweeps", "").replace(" ", ""))
             if 'no_of_observations' in row[0]:
                 no_of_observations = int(row[0].replace("no_of_observations", "").replace(" ", ""))
             if 'initial_temperature' in row[0]:
-                initial_temperature = float(row[0].replace("d0", "").replace("initial_temperature", "").replace(" ",
-                                                                                                                ""))
+                initial_temperature = float(row[0].replace("d0", "").replace("initial_temperature", "").replace(" ", ""))
             if 'final_temperature' in row[0]:
                 final_temperature = float(row[0].replace("d0", "").replace("final_temperature", "").replace(" ", ""))
             if 'no_of_temperature_increments' in row[0]:
-                no_of_temperature_increments = int(row[0].replace("no_of_temperature_increments", "").replace(" ",
-                                                                                                                  ""))
+                no_of_temperature_increments = int(row[0].replace("no_of_temperature_increments", "").replace(" ", ""))
     return (algorithm_name, output_directory, integer_lattice_length, no_of_equilibrium_iterations, no_of_observations,
             initial_temperature, final_temperature, no_of_temperature_increments)
