83ac41f32de126751bf0e9f10cc0d1214d6e084b
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-07-29 16:34:48 +0100

    polyspectra.py: extracted get_sampling_frequency() method from the two power spectrum methods

--- output/polyspectra.py
+++ output/polyspectra.py
@@ -14,16 +14,13 @@ sample_getter = importlib.import_module("sample_getter")
 
 def get_power_spectrum(power_spectrum_string, output_directory, temperature_directory, beta, no_of_sites,
                        no_of_equilibration_sweeps, sampling_frequency=None):
+    sampling_frequency = get_sampling_frequency(output_directory, sampling_frequency, temperature_directory)
     get_sample_method = getattr(sample_getter, "get_" + power_spectrum_string)
     sample = get_sample_method(output_directory, temperature_directory, beta, no_of_sites)[no_of_equilibration_sweeps:]
     # sample_variance = np.sum(np.var(np.atleast_2d(sample), axis=1))
     # the following line subtracts the Gaussian contribution ot the power spectrum,
     # though numerical experiments seem to indicate it's redundant
     sample -= np.mean(sample, axis=0)
-    if sampling_frequency is None:
-        acceptance_rates = sample_getter.get_acceptance_rates(output_directory, temperature_directory)
-        physical_time_scale = acceptance_rates[1] * acceptance_rates[0] ** 2 / 24.0
-        sampling_frequency = 1.0 / physical_time_scale
     sample = np.atleast_2d(sample)
     if len(sample) > 1:
         sample = sample.transpose()
@@ -35,15 +32,20 @@ def get_power_spectrum(power_spectrum_string, output_directory, temperature_dire
 
 def get_correlator_power_spectrum(power_spectrum_string, output_directory, temperature_directory, beta, no_of_sites,
                                   no_of_equilibration_sweeps, sampling_frequency=None, shifted_time_period=None):
+    sampling_frequency = get_sampling_frequency(output_directory, sampling_frequency, temperature_directory)
     get_sample_method = getattr(sample_getter, "get_" + power_spectrum_string)
     sample = get_sample_method(output_directory, temperature_directory, beta, no_of_sites)[no_of_equilibration_sweeps:]
     relative_sample = sample - np.mean(sample)
     if shifted_time_period is None:
         shifted_time_period = int(0.1 * len(sample))
     shifted_relative_sample = np.roll(relative_sample, shifted_time_period)
+    two_point_correlator = np.conj(relative_sample) * shifted_relative_sample
+    return signal.periodogram(two_point_correlator, fs=sampling_frequency)
+
+
+def get_sampling_frequency(output_directory, sampling_frequency, temperature_directory):
     if sampling_frequency is None:
         acceptance_rates = sample_getter.get_acceptance_rates(output_directory, temperature_directory)
         physical_time_scale = acceptance_rates[1] * acceptance_rates[0] ** 2 / 24.0
         sampling_frequency = 1.0 / physical_time_scale
-    two_point_correlator = np.conj(relative_sample) * shifted_relative_sample
-    return signal.periodogram(two_point_correlator, fs=sampling_frequency)
+    return sampling_frequency
