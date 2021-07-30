ac2395021bd27d3e0a500b31ec93246ea317064b
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-07-30 15:18:47 +0100

    polyspectra.py: exported more code to get_mean_zero_time_series()

--- output/polyspectra.py
+++ output/polyspectra.py
@@ -14,20 +14,18 @@ sample_getter = importlib.import_module("sample_getter")
 def get_power_spectrum(power_spectrum_string, output_directory, temperature_directory, beta, no_of_sites,
                        no_of_equilibration_sweeps, sampling_frequency=None):
     sampling_frequency = get_sampling_frequency(output_directory, sampling_frequency, temperature_directory)
-    get_sample_method = getattr(sample_getter, "get_" + power_spectrum_string)
-    sample = get_sample_method(output_directory, temperature_directory, beta, no_of_sites)[no_of_equilibration_sweeps:]
-    # sample_variance = np.sum(np.var(np.atleast_2d(sample), axis=1))
-    return get_normalised_component_averaged_power_spectrum(get_mean_zero_time_series(sample), sampling_frequency)
+    mean_zero_time_series = get_mean_zero_time_series(power_spectrum_string, output_directory, temperature_directory,
+                                                      beta, no_of_sites, no_of_equilibration_sweeps)
+    return get_normalised_component_averaged_power_spectrum(mean_zero_time_series, sampling_frequency)
 
 
 def get_correlator_power_spectrum(power_spectrum_string, output_directory, temperature_directory, beta, no_of_sites,
                                   no_of_equilibration_sweeps, sampling_frequency=None, shifted_time_period=None):
     sampling_frequency = get_sampling_frequency(output_directory, sampling_frequency, temperature_directory)
-    get_sample_method = getattr(sample_getter, "get_" + power_spectrum_string)
-    sample = get_sample_method(output_directory, temperature_directory, beta, no_of_sites)[no_of_equilibration_sweeps:]
-    mean_zero_time_series = get_mean_zero_time_series(sample)
+    mean_zero_time_series = get_mean_zero_time_series(power_spectrum_string, output_directory, temperature_directory,
+                                                      beta, no_of_sites, no_of_equilibration_sweeps)
     if shifted_time_period is None:
-        shifted_time_period = int(0.1 * len(sample))
+        shifted_time_period = int(0.1 * len(mean_zero_time_series[0]))
     shifted_mean_zero_time_series = np.roll(mean_zero_time_series, shifted_time_period, axis=1)
     two_point_correlator = np.conj(mean_zero_time_series) * shifted_mean_zero_time_series
     return get_normalised_component_averaged_power_spectrum(two_point_correlator, sampling_frequency)
@@ -41,7 +39,10 @@ def get_sampling_frequency(output_directory, sampling_frequency, temperature_dir
     return sampling_frequency
 
 
-def get_mean_zero_time_series(sample):
+def get_mean_zero_time_series(power_spectrum_string, output_directory, temperature_directory, beta, no_of_sites,
+                              no_of_equilibration_sweeps):
+    get_sample_method = getattr(sample_getter, "get_" + power_spectrum_string)
+    sample = get_sample_method(output_directory, temperature_directory, beta, no_of_sites)[no_of_equilibration_sweeps:]
     mean_zero_sample = sample - np.mean(sample, axis=0)
     mean_zero_sample = np.atleast_2d(mean_zero_sample)
     if len(mean_zero_sample) > 1:
