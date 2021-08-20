3931b49b031ed928c2fd696a1f2ac6435087ef22
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-08-20 11:59:35 +0100

    create_trispectrum_estimator_comparisons.py: power_spectrum_string -> observable_string

--- output/create_trispectrum_estimator_comparisons.py
+++ output/create_trispectrum_estimator_comparisons.py
@@ -14,7 +14,7 @@ markov_chain_diagnostics = importlib.import_module("markov_chain_diagnostics")
 polyspectra = importlib.import_module("polyspectra")
 
 
-def main(config_file, power_spectrum_string):
+def main(config_file, observable_string):
     basic_config_data = config_data_getter.get_basic_data(config_file)
     (algorithm_name, output_directory, integer_lattice_length, no_of_equilibration_sweeps, initial_temperature,
      final_temperature, no_of_temperature_increments, no_of_jobs) = (basic_config_data[0], basic_config_data[1],
@@ -22,7 +22,7 @@ def main(config_file, power_spectrum_string):
                                                                      basic_config_data[5], basic_config_data[6],
                                                                      basic_config_data[7], basic_config_data[8])
 
-    check_for_config_errors(algorithm_name, power_spectrum_string)
+    check_for_config_errors(algorithm_name, observable_string)
     no_of_sites = integer_lattice_length ** 2
     temperature = final_temperature
     if no_of_temperature_increments == 0:
@@ -44,11 +44,11 @@ def main(config_file, power_spectrum_string):
         beta = 1.0 / temperature
         temperature_directory = f"temp_eq_{temperature:.2f}"
 
-        power_trispectrum = polyspectra.get_power_trispectrum(power_spectrum_string, output_directory,
+        power_trispectrum = polyspectra.get_power_trispectrum(observable_string, output_directory,
                                                               temperature_directory, beta, no_of_sites,
                                                               no_of_equilibration_sweeps, no_of_jobs, pool,
                                                               no_of_trispectrum_octaves, trispectrum_base_period_shift)
-        power_trispectrum_direct = polyspectra.get_power_trispectrum_direct(power_spectrum_string, output_directory,
+        power_trispectrum_direct = polyspectra.get_power_trispectrum_direct(observable_string, output_directory,
                                                                             temperature_directory, beta, no_of_sites,
                                                                             no_of_equilibration_sweeps, no_of_jobs,
                                                                             pool, no_of_trispectrum_octaves,
@@ -114,7 +114,7 @@ def main(config_file, power_spectrum_string):
         for legend in trispectrum_legend:
             legend.get_frame().set_edgecolor("k")
             legend.get_frame().set_lw(1.5)
-        figure.savefig(f"{output_directory}/{power_spectrum_string}_compare_power_trispectrum_functions"
+        figure.savefig(f"{output_directory}/{observable_string}_compare_power_trispectrum_functions"
                        f"_temp_eq_{temperature:.2f}.pdf", bbox_inches="tight")
         figure.clf()
         temperature -= magnitude_of_temperature_increments
@@ -123,11 +123,11 @@ def main(config_file, power_spectrum_string):
         pool.close()
 
 
-def check_for_config_errors(algorithm_name, power_spectrum_string):
+def check_for_config_errors(algorithm_name, observable_string):
     if ((algorithm_name == "elementary-electrolyte" or algorithm_name == "multivalued-electrolyte") and
-            (power_spectrum_string == "magnetisation_norm" or power_spectrum_string == "magnetisation_norm" or
-             power_spectrum_string == "helicity_modulus" or power_spectrum_string == "inverse_vacuum_permittivity" or
-             power_spectrum_string == "toroidal_vortex_polarisation")):
+            (observable_string == "magnetisation_norm" or observable_string == "magnetisation_norm" or
+             observable_string == "helicity_modulus" or observable_string == "inverse_vacuum_permittivity" or
+             observable_string == "toroidal_vortex_polarisation")):
         print("ConfigurationError: This is an Maggs-electrolyte model: do not give either magnetisation_norm, "
               "magnetisation_phase, helicity_modulus, inverse_vacuum_permittivity or toroidal_vortex_polarisation as "
               "the second positional argument.")
@@ -135,9 +135,9 @@ def check_for_config_errors(algorithm_name, power_spectrum_string):
     if ((algorithm_name == "xy-ecmc" or algorithm_name == "hxy-ecmc" or algorithm_name == "xy-metropolis" or
          algorithm_name == "hxy-metropolis" or algorithm_name == "xy-gaussian-noise-metropolis" or
          algorithm_name == "hxy-gaussian-noise-metropolis") and (
-            power_spectrum_string == "inverse_permittivity" or
-            power_spectrum_string == "topological_sector_fluctuations" or
-            power_spectrum_string == "toroidal_polarisation")):
+            observable_string == "inverse_permittivity" or
+            observable_string == "topological_sector_fluctuations" or
+            observable_string == "toroidal_polarisation")):
         print("ConfigurationError: This is an XY or HXY model: do not give either inverse_permittivity, "
               "topological_sector_fluctuations or toroidal_polarisation as the second positional argument.")
         exit()
