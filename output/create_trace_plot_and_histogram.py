8f0fd65cb9f9de3e327efb9c6ffb67fb9e799ea3
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-08-26 16:17:02 +0100

    create_trace_plot_and_histogram.py: added iteration over temperatures

--- output/create_trace_plot_and_histogram.py
+++ output/create_trace_plot_and_histogram.py
@@ -19,36 +19,40 @@ def main(config_file, observable_string, length_of_trace_plot=1000, number_of_hi
     (algorithm_name, output_directory, no_of_sites, no_of_equilibration_sweeps, initial_temperature,
      final_temperature, no_of_temperature_increments, no_of_jobs) = config_data_getter.get_basic_data(config_file)
     config_data_getter.check_for_observable_error(algorithm_name, observable_string)
+    (temperature, magnitude_of_temperature_increments) = config_data_getter.get_temperature_and_magnitude_of_increments(
+        initial_temperature, final_temperature, no_of_temperature_increments)
     if no_of_jobs != 1:
         sample_directory = output_directory + f"/job_{random.choice(range(no_of_jobs)) + 1}"
     else:
         sample_directory = output_directory
 
-    # todo iterate over temperatures
-    temperature = final_temperature
+    for i in range(no_of_temperature_increments + 1):
+        beta = 1.0 / temperature
+        temperature_directory = f"temp_eq_{temperature:.2f}"
+        get_sample_method = getattr(sample_getter, "get_" + observable_string)
+        sample = np.atleast_2d(get_sample_method(sample_directory, temperature_directory, beta, no_of_sites))
 
-    beta = 1.0 / temperature
-    temperature_directory = f"temp_eq_{temperature:.2f}"
-    get_sample_method = getattr(sample_getter, "get_" + observable_string)
-    sample = np.atleast_2d(get_sample_method(sample_directory, temperature_directory, beta, no_of_sites))
+        plt.xlabel(r"time, $t$", fontsize=15, labelpad=10)
+        plt.ylabel(r"$ x \left( t \right)$", fontsize=15, labelpad=10)
+        plt.tick_params(axis="both", which="major", labelsize=14, pad=10)
+        plt.plot(sample[0, no_of_equilibration_sweeps:no_of_equilibration_sweeps + length_of_trace_plot], color="k",
+                 linewidth=1, linestyle="-")
+        plt.tight_layout()
+        plt.savefig(f"{output_directory}/{observable_string}_vs_time_temp_eq_{temperature:.2f}_"
+                    f"{int(no_of_sites ** 0.5)}_{int(no_of_sites ** 0.5)}_{algorithm_name.replace('-', '_')}.pdf",
+                    bbox_inches="tight")
+        plt.clf()
 
-    plt.xlabel(r"time, $t$", fontsize=15, labelpad=10)
-    plt.ylabel(r"$ x \left( t \right)$", fontsize=15, labelpad=10)
-    plt.tick_params(axis="both", which="major", labelsize=14, pad=10)
-    plt.plot(sample[0, no_of_equilibration_sweeps:no_of_equilibration_sweeps + length_of_trace_plot], color="k",
-             linewidth=1, linestyle="-")
-    plt.tight_layout()
-    plt.savefig(f"{output_directory}/{observable_string}_vs_time_{int(no_of_sites ** 0.5)}_"
-                f"{int(no_of_sites ** 0.5)}_{algorithm_name.replace('-', '_')}.pdf", bbox_inches="tight")
-    plt.clf()
+        plt.xlabel(r"$x$", fontsize=15, labelpad=10)
+        plt.ylabel(r"$ \mathbb{P} \left( |x| < |X| < |x| + |dx| \right)$", fontsize=15, labelpad=10)
+        plt.tick_params(axis="both", which="major", labelsize=14, pad=10)
+        plt.hist(sample[0, no_of_equilibration_sweeps:] - np.mean(sample[0, no_of_equilibration_sweeps:]),
+                 bins=number_of_histogram_bins)
+        plt.savefig(f"{output_directory}/{observable_string}_histogram_temp_eq_{temperature:.2f}_"
+                    f"{int(no_of_sites ** 0.5)}_{int(no_of_sites ** 0.5)}_{algorithm_name.replace('-', '_')}.pdf",
+                    bbox_inches="tight")
 
-    plt.xlabel(r"$x$", fontsize=15, labelpad=10)
-    plt.ylabel(r"$ \mathbb{P} \left( |x| < |X| < |x| + |dx| \right)$", fontsize=15, labelpad=10)
-    plt.tick_params(axis="both", which="major", labelsize=14, pad=10)
-    plt.hist(sample[0, no_of_equilibration_sweeps:] - np.mean(sample[0, no_of_equilibration_sweeps:]),
-             bins=number_of_histogram_bins)
-    plt.savefig(f"{output_directory}/{observable_string}_histogram_{int(no_of_sites ** 0.5)}_"
-                f"{int(no_of_sites ** 0.5)}_{algorithm_name.replace('-', '_')}.pdf", bbox_inches="tight")
+        temperature = temperature - magnitude_of_temperature_increments
 
 
 if __name__ == "__main__":
