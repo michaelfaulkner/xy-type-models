64e3f7f6d1caee0bcb3f3f9d902d081ff7949928
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-10-21 12:51:35 +0100

    added sample_getter.get_non_normalised_toroidal_vortex_polarisation() to simplify some repeated code

--- output/sample_getter.py
+++ output/sample_getter.py
@@ -40,25 +40,38 @@ def get_specific_heat(output_directory, temperature_directory, beta, no_of_sites
 
 
 # xy models
-def get_helicity_modulus(output_directory, temperature_directory, beta, no_of_sites):
-    sum_of_1st_derivative_of_potential_sample = get_entire_sample(output_directory, temperature_directory)[:, 3:5]
-    return (get_inverse_vacuum_permittivity(output_directory, temperature_directory, beta, no_of_sites) - beta
-            * np.mean((sum_of_1st_derivative_of_potential_sample - np.mean(sum_of_1st_derivative_of_potential_sample,
-                                                                           axis=0)) ** 2, axis=1) / no_of_sites)
+def get_non_normalised_toroidal_vortex_polarisation(output_directory, temperature_directory, beta, no_of_sites):
+    return get_entire_sample(output_directory, temperature_directory)[:, 3:5]
 
 
 def get_inverse_vacuum_permittivity(output_directory, temperature_directory, beta, no_of_sites):
     return np.mean(get_entire_sample(output_directory, temperature_directory)[:, 5:7], axis=1) / no_of_sites
 
 
+def get_helicity_modulus(output_directory, temperature_directory, beta, no_of_sites):
+    non_normalised_toroidal_vortex_polarisation = get_non_normalised_toroidal_vortex_polarisation(output_directory,
+                                                                                                  temperature_directory,
+                                                                                                  beta, no_of_sites)
+    return (get_inverse_vacuum_permittivity(output_directory, temperature_directory, beta, no_of_sites) - beta
+            * np.mean((non_normalised_toroidal_vortex_polarisation -
+                       np.mean(non_normalised_toroidal_vortex_polarisation, axis=0)) ** 2, axis=1) / no_of_sites)
+
+
 def get_toroidal_vortex_polarisation(output_directory, temperature_directory, beta, no_of_sites):
-    return get_entire_sample(output_directory, temperature_directory)[:, 3:5] / no_of_sites
+    return get_non_normalised_toroidal_vortex_polarisation(output_directory, temperature_directory, beta,
+                                                           no_of_sites) / no_of_sites
 
 
 def get_hxy_topological_sector(output_directory, temperature_directory, beta, no_of_sites):
-    sum_of_1st_derivative_of_potential_sample = get_entire_sample(output_directory, temperature_directory)[:, 3:5]
-    return (sum_of_1st_derivative_of_potential_sample + math.pi * no_of_sites ** 0.5) // (2.0 * math.pi
-                                                                                          * no_of_sites ** 0.5)
+    non_normalised_toroidal_vortex_polarisation = get_non_normalised_toroidal_vortex_polarisation(output_directory,
+                                                                                                  temperature_directory,
+                                                                                                  beta, no_of_sites)
+    return (non_normalised_toroidal_vortex_polarisation + math.pi * no_of_sites ** 0.5) // (2.0 * math.pi
+                                                                                            * no_of_sites ** 0.5)
+
+
+def get_non_normalised_cartesian_magnetisation(output_directory, temperature_directory, beta, no_of_sites):
+    return get_entire_sample(output_directory, temperature_directory)[:, 1:3]
 
 
 def get_magnetisation_norm(output_directory, temperature_directory, beta, no_of_sites):
@@ -75,10 +88,6 @@ def get_magnetisation_phase(output_directory, temperature_directory, beta, no_of
 def get_cartesian_magnetisation(output_directory, temperature_directory, beta, no_of_sites):
     return get_non_normalised_cartesian_magnetisation(output_directory, temperature_directory, beta,
                                                       no_of_sites) / no_of_sites
-
-
-def get_non_normalised_cartesian_magnetisation(output_directory, temperature_directory, beta, no_of_sites):
-    return get_entire_sample(output_directory, temperature_directory)[:, 1:3]
 
 
 def get_magnetic_susceptibility(output_directory, temperature_directory, beta, no_of_sites):
@@ -101,14 +110,14 @@ def get_inverse_permittivity(output_directory, temperature_directory, beta, no_o
                                 axis=1) / no_of_sites
 
 
+def get_topological_sector(output_directory, temperature_directory, beta, no_of_sites):
+    sum_of_electric_field_sample = get_sum_of_electric_field(output_directory, temperature_directory, beta, no_of_sites)
+    return (sum_of_electric_field_sample + math.pi * no_of_sites ** 0.5) // (2.0 * math.pi * no_of_sites ** 0.5)
+
+
 def get_topological_sector_fluctuations(output_directory, temperature_directory, beta, no_of_sites):
     # n.b. in PRB 91, 155412, topological_sector_fluctuations were defined w/out the factor of 1/2 to account for each
     # Cartesian dimension of the system; in the return line below, this corresponds to the first np.mean() -> np.sum()
     topological_sector_sample = get_topological_sector(output_directory, temperature_directory, beta, no_of_sites)
     return 4.0 * beta * math.pi ** 2 * np.mean((topological_sector_sample - np.mean(topological_sector_sample, axis=0))
                                                ** 2, axis=1)
-
-
-def get_topological_sector(output_directory, temperature_directory, beta, no_of_sites):
-    sum_of_electric_field_sample = get_sum_of_electric_field(output_directory, temperature_directory, beta, no_of_sites)
-    return (sum_of_electric_field_sample + math.pi * no_of_sites ** 0.5) // (2.0 * math.pi * no_of_sites ** 0.5)
