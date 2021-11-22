92bbf9c0c988564289f1089df595a695f6bd73ed
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-11-22 13:52:13 +0000

    create_polyspectra_plots.py: now only perform 1/f fitting at upper frequency range

--- sample_analysis/create_polyspectra_plots.py
+++ sample_analysis/create_polyspectra_plots.py
@@ -60,37 +60,30 @@ def main(config_file, observable_string, no_of_trispectrum_auxiliary_frequency_o
                                                     power_spectrum[1, :final_frequency_index],
                                                     bounds=(np.array([0.1, initial_frequency]),
                                                             np.array([10.0, final_frequency])))
-        parameter_values = parameter_values_and_errors[0]
-        parameter_errors = np.sqrt(np.diag(parameter_values_and_errors[1]))
-        model_frequency_values = np.arange(initial_frequency, final_frequency, increment)
-        model_spectrum_values = lorentzian_model(model_frequency_values, *parameter_values)
+        lorentzian_model_parameters = parameter_values_and_errors[0]
+        lorentzian_model_errors = np.sqrt(np.diag(parameter_values_and_errors[1]))
+        lorentzian_model_frequency_values = np.arange(initial_frequency, final_frequency, increment)
+        lorentzian_model_spectrum_values = lorentzian_model(lorentzian_model_frequency_values,
+                                                            *lorentzian_model_parameters)
         axes[0].loglog(power_spectrum[0, :max_power_spectrum_index], power_spectrum[1, :max_power_spectrum_index],
                        color=current_color,
-                       label=fr"temperature = {temperature:.2f}; $f_c$ = {parameter_values[1]:.2e} $\pm$ "
-                             fr"{parameter_errors[1]:.2e}; $S_0$ = {parameter_values[0]:.2f} $\pm$ "
-                             fr"{parameter_errors[0]:.2e}")
-        axes[0].loglog(model_frequency_values, model_spectrum_values, color='k')
+                       label=fr"temperature = {temperature:.2f}; $f_c$ = {lorentzian_model_parameters[1]:.2e} $\pm$ "
+                             fr"{lorentzian_model_errors[1]:.2e}; $S_0$ = {lorentzian_model_parameters[0]:.2f} $\pm$ "
+                             fr"{lorentzian_model_errors[0]:.2e}")
+        axes[0].loglog(lorentzian_model_frequency_values, lorentzian_model_spectrum_values, color='k')
 
-        """lower frequency range of trispectrum fitting"""
-        (one_over_f_model_lower_parameters, one_over_f_model_lower_errors, one_over_f_model_lower_frequency_values,
-         one_over_f_model_lower_spectrum_values) = fit_one_over_f_model_to_trispectrum(power_trispectrum, "lower", 10.0,
-                                                                                       no_of_sites, no_of_jobs)
-        """upper frequency range of trispectrum fitting"""
-        (one_over_f_model_upper_parameters, one_over_f_model_upper_errors, one_over_f_model_upper_frequency_values,
-         one_over_f_model_upper_spectrum_values) = fit_one_over_f_model_to_trispectrum(power_trispectrum, "upper", 10.0,
-                                                                                       no_of_sites, no_of_jobs)
+        """fit 1/f model to trispectrum"""
+        (one_over_f_model_parameters, one_over_f_model_errors, one_over_f_model_frequency_values,
+         one_over_f_model_spectrum_values) = fit_one_over_f_model_to_trispectrum(power_trispectrum, "upper", 10.0,
+                                                                                 no_of_sites, no_of_jobs)
         axes[1].loglog(power_trispectrum[1][:max_power_trispectrum_index],
                        power_trispectrum[2][len(power_trispectrum[2]) - 1][:max_power_trispectrum_index],
                        color=current_color,
                        label=fr"temperature = {temperature:.2f}; "
                              fr"f' = {power_trispectrum[0][len(power_trispectrum[0]) - 1]:.2e}; "
-                             fr"$\alpha_L$ = {one_over_f_model_lower_parameters[1]:.2e} $\pm$ "
-                             fr"{one_over_f_model_lower_errors[1]:.2e}; "
-                             fr"$\alpha_U$ = {one_over_f_model_upper_parameters[1]:.2e} $\pm$ "
-                             fr"{one_over_f_model_upper_errors[1]:.2e}")
-        axes[1].loglog(one_over_f_model_lower_frequency_values, one_over_f_model_lower_spectrum_values, color='k')
-        axes[1].loglog(one_over_f_model_upper_frequency_values, one_over_f_model_upper_spectrum_values, color='k',
-                       linestyle='dashed')
+                             fr"$\alpha$ = {one_over_f_model_parameters[1]:.2e} $\pm$ "
+                             fr"{one_over_f_model_errors[1]:.2e}")
+        axes[1].loglog(one_over_f_model_frequency_values, one_over_f_model_spectrum_values, color='k')
 
         temperature -= magnitude_of_temperature_increments
     print(f"Sample analysis complete.  Total runtime = {time.time() - start_time:.2e} seconds.")
@@ -99,8 +92,8 @@ def main(config_file, observable_string, no_of_trispectrum_auxiliary_frequency_o
     if observable_string == "cartesian_magnetisation":
         legends = [axes[0].legend(title=r"black lines: fits to $S_X(f)$ / $S_X(f_0) = S_0 / (1 + (f / f_c)^2)$",
                                   loc="lower left", fontsize=10),
-                   axes[1].legend(title=r"black lines: fits to $S_X^3(f, f') \sim f^{-\alpha}$; $\alpha = \alpha_L$ "
-                                        r"(solid), $\alpha = \alpha_U$ (dashed)", loc="lower left", fontsize=8)]
+                   axes[1].legend(title=r"black lines: fits to $S_X^3(f, f') \sim f^{-\alpha}$", loc="lower left",
+                                  fontsize=8)]
     else:
         legends = [axis.legend(loc="lower left", fontsize=10) for axis in axes]
     [legend.get_frame().set_edgecolor("k") for legend in legends]
