89ba41e78eff387acedc37c8e9d2b6116efc06af
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2022-01-31 13:20:00 +0000

    introduced 'try: ... except ModuleNotFoundError: ...' functionality to markov_chain_diagnostics.py pour definir get_effective_sample_size() and get_sample_mean_and_error() based on the availability of rpy2

--- sample_analysis/markov_chain_diagnostics.py
+++ sample_analysis/markov_chain_diagnostics.py
@@ -1,16 +1,35 @@
 import numpy as np
-import rpy2.robjects.numpy2ri as n2ri
-import rpy2.robjects.packages as r_packages
-n2ri.activate()
-laplaces_demon_r_package = r_packages.importr('LaplacesDemon')
-mcmcse_r_package = r_packages.importr('mcmcse')
+
+try:
+    import rpy2.robjects.numpy2ri as n2ri
+    import rpy2.robjects.packages as r_packages
+    n2ri.activate()
+    laplaces_demon_r_package = r_packages.importr('LaplacesDemon')
+    mcmcse_r_package = r_packages.importr('mcmcse')
 
 
-def get_effective_sample_size(one_dimensional_sample):
-    if len(np.atleast_2d(one_dimensional_sample)) > 1:
-        raise Exception("Error: the sample passed to markov_chain_diagnostics.get_effective_sample_size() must be one "
-                        "(Cartesian) dimensional.")
-    return np.array(laplaces_demon_r_package.ESS(one_dimensional_sample))[0]
+    def get_effective_sample_size(one_dimensional_sample):
+        if len(np.atleast_2d(one_dimensional_sample)) > 1:
+            raise Exception("Error: the sample passed to markov_chain_diagnostics.get_effective_sample_size() must be "
+                            "one (Cartesian) dimensional.")
+        return np.array(laplaces_demon_r_package.ESS(one_dimensional_sample))[0]
+
+
+    def get_sample_mean_and_error(one_dimensional_sample):
+        if len(np.atleast_2d(one_dimensional_sample)) > 1:
+            raise Exception("Error: the sample passed to markov_chain_diagnostics.get_sample_mean_and_error() must be "
+                            "one (Cartesian) dimensional.")
+        sample_mean_and_error = np.array(mcmcse_r_package.mcse(one_dimensional_sample))
+        return sample_mean_and_error[0, 0], sample_mean_and_error[1, 0]
+
+except ModuleNotFoundError:
+    def get_effective_sample_size(_):
+        print("rpy2 not available: get_effective_sample_size() returns None.")
+        return None
+
+
+    def get_sample_mean_and_error(sample):
+        return np.mean(sample), np.std(sample) / len(sample) ** 0.5
 
 
 def get_thinned_sample(one_dimensional_sample, thinning_level):
@@ -19,14 +38,6 @@ def get_thinned_sample(one_dimensional_sample, thinning_level):
                         "(Cartesian) dimensional.")
     sample_indices_to_keep = np.array([i for i in range(len(one_dimensional_sample)) if i % thinning_level == 0])
     return np.take(one_dimensional_sample, sample_indices_to_keep)
-
-
-def get_sample_mean_and_error(one_dimensional_sample):
-    if len(np.atleast_2d(one_dimensional_sample)) > 1:
-        raise Exception("Error: the sample passed to markov_chain_diagnostics.get_sample_mean_and_error() must be one "
-                        "(Cartesian) dimensional.")
-    sample_mean_and_error = np.array(mcmcse_r_package.mcse(one_dimensional_sample))
-    return sample_mean_and_error[0, 0], sample_mean_and_error[1, 0]
 
 
 def get_cumulative_distribution(one_dimensional_sample):
