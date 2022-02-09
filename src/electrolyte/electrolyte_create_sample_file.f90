136f2c2cb9e965b6d70b0948ccd7dc364dd3cbb2
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2022-02-09 12:32:53 +0000

    src: some formatting changes related to external_global_move

--- src/electrolyte/electrolyte_create_sample_file.f90
+++ src/electrolyte/electrolyte_create_sample_file.f90
@@ -14,8 +14,9 @@ open(unit=20, file=filename)
 
 write(20, '(A1, A23, "; ", I0.4, " x ", I0.4, " lattice no_of_sites; temperature = ", ES8.2)') "#", &
                                         algorithm_name, integer_lattice_length, integer_lattice_length, temperature
-write(20, '(A1, A29, 4A30)') "#", "potential", "sum_of_electric_field_x", "sum_of_electric_field_y", &
-                                    "external_global_move_x", "external_global_move_y"
+write(20, '(A1, A29, 4A30)') "#", "potential", "external_global_move_x", "external_global_move_y", &
+                                    "sum_of_electric_field_x", "sum_of_electric_field_y"
+
 
 return
 end subroutine create_sample_file
--- src/xy_models/xy/xy_get_and_print_observation.f90
+++ src/xy_models/xy/xy_get_and_print_observation.f90
@@ -25,13 +25,13 @@ do i = 1, no_of_sites
                           - cos(spin_field(get_north_neighbour(i)) - spin_field(i))
 end do
 
-write(20, 100) potential, non_normalised_magnetisation(1), non_normalised_magnetisation(2), &
+write(20, 100) potential, external_global_move(1), external_global_move(2), &
+                non_normalised_magnetisation(1), non_normalised_magnetisation(2), &
                 sum_of_1st_derivative_of_potential(1), sum_of_1st_derivative_of_potential(2), &
-                sum_of_2nd_derivative_of_potential(1), sum_of_2nd_derivative_of_potential(2), &
-                external_global_move(1), external_global_move(2)
+                sum_of_2nd_derivative_of_potential(1), sum_of_2nd_derivative_of_potential(2)
 
-100 format(ES30.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", I29, &
-            ",", I29)
+100 format(ES30.14, ",", I29, ",", I29, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", ES29.14, ",", &
+            ES29.14)
 
 return
 end subroutine get_and_print_observation
