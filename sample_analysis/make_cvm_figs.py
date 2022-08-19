561fbc81541361fc5bbbc59edd79a4555b5c7356
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2022-08-19 16:24:15 +0100

    make_cvm_figs.py: CvM analysis working nicely and formatted - partial use of errors but np.polyfit doesn't produce its own error estimates at the chosen polynomial-fitting degree

--- sample_analysis/make_cvm_figs.py
+++ sample_analysis/make_cvm_figs.py
@@ -57,49 +57,48 @@ def main(no_of_system_sizes=6):
     temperatures_metrop = [*temperatures_metrop_low_temps, *temperatures_metrop_lower_trans,
                            *temperatures_metrop_upper_trans, *temperatures_metrop_high_temps]
     temperatures_around_transition = temperatures_metrop[3:15]
-    continuous_temperatures = np.linspace(temperatures_metrop[3], temperatures_metrop[14], 100)
     approx_transition_temperature = 0.887
     reduced_temperatures_metrop = [temperature / approx_transition_temperature for temperature in temperatures_metrop]
     reduced_temperatures_ecmc = [temperature / approx_transition_temperature for temperature in temperatures_ecmc]
 
-    fig, axes = plt.subplots(1, 2, figsize=(10.0, 4.2), gridspec_kw={'width_ratios': [1.2, 1.0]})
-    fig.text(-0.015, 0.9, "d", fontsize=20, weight='bold')
-    fig.text(0.535, 0.9, "e", fontsize=20, weight='bold')
+    fig, axes = plt.subplots(1, 2, figsize=(10.0, 3.75), gridspec_kw={'width_ratios': [1.2, 1.0]})
+    fig.text(-0.025, 0.9, "d", fontsize=20, weight='bold')
+    fig.text(0.525, 0.9, "e", fontsize=20, weight='bold')
     fig.tight_layout(w_pad=3.0)
     axes[0].set_yscale('log')
     axes[0].set_xlabel(r"$\widetilde{\beta}_{\rm BKT} / \beta$", fontsize=20, labelpad=-0.5)
     axes[0].set_ylabel(r"$n \omega_{\phi_m,n}^2$", fontsize=20, labelpad=-7.5)
     axes[0].set_ylim([2.0e-4, 2.5e5])
     axes[0].set_yticks([10.0 ** index for index in range(-3, 6)])
-    axes[1].set_xlabel(r"$\left( \ln N \right)^{-2}$", fontsize=20, labelpad=4)
+    axes[1].set_xlabel(r"$1 / \left( \ln N \right)^2$", fontsize=20, labelpad=4)
     axes[1].set_ylabel(r"$\widetilde{\beta}_{\rm BKT} / \beta_{\rm int}$", fontsize=20, labelpad=-0.5)
-    axes[1].set_xlim([0.0, 0.06]), axes[1].set_ylim([0.975, 1.75])
-    [axis.spines[spine].set_linewidth(3) for spine in ["top", "bottom", "left", "right"] for axis in axes]
+    axes[1].set_xlim([0.0, 0.06]), axes[1].set_ylim([0.975, 1.65])
+    [axis.spines[spine].set_linewidth(3.0) for spine in ["top", "bottom", "left", "right"] for axis in axes]
     for axis_index, axis in enumerate(axes):
-        axis.tick_params(which='major', direction='in', width=3, length=5, labelsize=14, pad=2.5)
+        axis.tick_params(which='major', direction='in', width=2.5, length=5, labelsize=18, pad=2.5)
         axis.tick_params(which='minor', direction='in', width=1.5, length=4)
 
     twinned_axis = axes[1].twinx()
-    twinned_axis.set_ylabel(r"$1 / \left(n \omega_{\phi_m,n}^2 \right) \left(\beta = \beta_{\rm int} \right)$",
-                            fontsize=20, labelpad=-0.5, color="red")
-    twinned_axis.set_yticks([0.0, 0.1, 0.2])
-    twinned_axis.set_ylim([-0.0068, 0.205])
-    plt.hlines(0.0, 0.001125, 0.06, colors="red", linestyles='--')
-    twinned_axis.tick_params(which='major', direction='in', width=3, length=5, labelsize=14, pad=2.5, colors="red")
+    twinned_axis.set_ylabel(r"$1 / \left(n \omega_{\phi_m,n}^2 \left(\beta = \beta_{\rm int} \right) \right)$",
+                            fontsize=16, labelpad=-0.5, color="red")
+    twinned_axis.set_yticks([0.0, 0.1, 0.2, 0.3])
+    twinned_axis.set_ylim([-0.0125, 0.32])
+    plt.hlines(0.0, 0.001125, 0.06, colors="red", linestyles='-')
+    twinned_axis.tick_params(which='major', direction='in', width=2.5, length=5, labelsize=18, pad=2.5, colors="red")
     twinned_axis.tick_params(which='minor', direction='in', width=1.5, length=4)
     twinned_axis.tick_params(axis='y', labelcolor='red')
-    twinned_axis.spines["right"].set_color("red"), twinned_axis.spines["right"].set_linewidth(3)
+    twinned_axis.spines["right"].set_color("red"), twinned_axis.spines["right"].set_linewidth(3.0)
 
-    inset_axis = plt.axes([0.6, 0.56, 0.21, 0.37])
+    inset_axis = plt.axes([0.6, 0.55, 0.21, 0.37])
     inset_axis.tick_params(which='major', direction='in', width=2, length=4, labelsize=12, pad=3.5)
     inset_axis.tick_params(which='minor', direction='in', width=0.25, length=3)
     inset_axis.yaxis.set_label_position("right"), inset_axis.yaxis.tick_right()
     inset_axis.set_xlabel(r"$\widetilde{\beta}_{\rm BKT} / \beta$", fontsize=14, labelpad=1.0)
-    inset_axis.set_ylabel(r"$\left(n \omega_{\phi_m,n}^2 \right)^{-1}$", fontsize=14, labelpad=1.5)
+    inset_axis.set_ylabel(r"$1 / \left(n \omega_{\phi_m,n}^2 \right)$", fontsize=14, labelpad=1.5)
     inset_axis.set_xlim([0.98, 1.54]), inset_axis.set_ylim([2.0 * 10 ** (-5), 1.5])
     inset_axis.set_yscale('log')
     inset_axis.set_yticks([1.0e-4, 1.0e-3, 1.0e-2, 1.0e-1, 1.0])
-    [inset_axis.spines[spine].set_linewidth(3) for spine in ["top", "bottom", "left", "right"]]
+    [inset_axis.spines[spine].set_linewidth(3.0) for spine in ["top", "bottom", "left", "right"]]
 
     colors = ["black", "red", "blue", "green", "magenta", "indigo"][:no_of_system_sizes]
     colors.reverse()
@@ -175,24 +174,15 @@ def main(no_of_system_sizes=6):
         cvm_errors_metrop = np.array([*cvm_errors_metrop_low_temps, *cvm_errors_metrop_lower_trans,
                                       *cvm_errors_metrop_upper_trans, *cvm_errors_metrop_high_temps])
 
-        if system_size_index == 5:
-            axes[0].errorbar(reduced_temperatures_metrop[1:], cvms_metrop[1:], cvm_errors_metrop[1:], marker=".",
-                             markersize=10, color=colors[system_size_index], linestyle="None",
-                             label=fr"$N$ = {length}x{length} Metrop.")
-        else:
-            axes[0].errorbar(reduced_temperatures_metrop, cvms_metrop, cvm_errors_metrop, marker=".", markersize=10,
-                             color=colors[system_size_index], linestyle="None",
-                             label=fr"$N$ = {length}x{length} Metrop.")
-        inset_axis.plot(reduced_temperatures_metrop, 1.0 / cvms_metrop, marker=".", markersize=10,
-                        color=colors[system_size_index], linestyle="None")
-        axes[0].errorbar(reduced_temperatures_ecmc, cvms_ecmc, cvm_errors_ecmc, marker="*", markersize=8,
-                         color=colors[system_size_index], linestyle="None", label=fr"$N$ = {length}x{length} ECMC")
-
+        """estimate intercept temperatures"""
         inverse_cvms_around_transition = 1.0 / cvms_metrop[3:15]
-        # cvm_errors_metrop_around_transition = np.array(cvm_errors_metrop[3:15])
+        inverse_cvm_errors_around_transition = cvm_errors_metrop[3:15] * inverse_cvms_around_transition ** 2
         if system_size_index > 0:
             current_fitting_inverse_cvms = inverse_cvms_around_transition[lower_fitting_index:upper_fitting_index]
-            current_polynomial_fit = np.poly1d(np.polyfit(fitting_temps, np.log(current_fitting_inverse_cvms), 2))
+            current_fitting_inverse_cvm_errors = inverse_cvm_errors_around_transition[lower_fitting_index:
+                                                                                      upper_fitting_index]
+            current_polynomial_fit = np.poly1d(np.polyfit(fitting_temps, np.log(current_fitting_inverse_cvms), 2,
+                                                          w=np.log(current_fitting_inverse_cvm_errors)))
             continuous_temperatures = np.linspace(fitting_temps[0], fitting_temps[-1], 100)
             '''inset_axis.plot(continuous_temperatures / approx_transition_temperature,
                             np.exp(previous_polynomial_fit(continuous_temperatures)), color=colors[system_size_index - 1],
@@ -222,27 +212,54 @@ def main(no_of_system_sizes=6):
             lower_fitting_index, upper_fitting_index = 0, 3
         fitting_temps = temperatures_around_transition[lower_fitting_index:upper_fitting_index]
         previous_fitting_inverse_cvms = inverse_cvms_around_transition[lower_fitting_index:upper_fitting_index]
-        previous_polynomial_fit = np.poly1d(np.polyfit(fitting_temps, np.log(previous_fitting_inverse_cvms), 2))
-        # print(np.polyfit(fitting_temps, previous_fitting_inverse_cvms, 2))
+        previous_fitting_inverse_cvm_errors = inverse_cvm_errors_around_transition[lower_fitting_index:
+                                                                                   upper_fitting_index]
+        previous_polynomial_fit = np.poly1d(np.polyfit(
+            fitting_temps, np.log(previous_fitting_inverse_cvms), 2,
+            w=previous_fitting_inverse_cvm_errors / previous_fitting_inverse_cvms))
+        '''popt, pcov = curve_fit(quadratic_fit, fitting_temps, np.log(previous_fitting_inverse_cvms),
+                                       sigma=previous_fitting_inverse_cvm_errors / previous_fitting_inverse_cvms)'''
 
-        '''polynomial_model = np.poly1d(np.polyfit(temperatures_around_transition, cvms_metrop_around_transition, 11))
-        inset_axis.plot(continuous_temperatures / approx_transition_temperature,
-                        polynomial_model(continuous_temperatures), color=colors[system_size_index], linestyle="--")'''
+        """plot CvM data"""
+        if system_size_index == 5:
+            """lowest-temperature results did not converge for L = 128"""
+            axes[0].errorbar(reduced_temperatures_metrop[1:], cvms_metrop[1:], cvm_errors_metrop[1:], marker=".",
+                             markersize=10, color=colors[system_size_index], linestyle="None",
+                             label=fr"$N$ = {length}x{length} Metrop.")
+        else:
+            axes[0].errorbar(reduced_temperatures_metrop, cvms_metrop, cvm_errors_metrop, marker=".", markersize=10,
+                             color=colors[system_size_index], linestyle="None",
+                             label=fr"$N$ = {length}x{length} Metrop.")
+        inset_axis.plot(reduced_temperatures_metrop, 1.0 / cvms_metrop, marker=".", markersize=10,
+                        color=colors[system_size_index], linestyle="None")
+        axes[0].errorbar(reduced_temperatures_ecmc, cvms_ecmc, cvm_errors_ecmc, marker="*", markersize=8,
+                         color=colors[system_size_index], linestyle="None", label=fr"$N$ = {length}x{length} ECMC")
 
-    inverse_system_sizes = [1.0 / np.log(length ** 2) ** 2 for length in linear_system_sizes]
-    # inverse_system_sizes.append(0.0), reduced_intersect_temperatures.append(1.0)
-    inverse_system_sizes.reverse(), reduced_intersect_temperatures.reverse(), intersect_values.reverse()
-    inverse_system_sizes.pop()
-    axes[1].plot(inverse_system_sizes, reduced_intersect_temperatures, marker=".", markersize=8, color="black",
-                 linestyle="None")
-    continuous_reduced_intersect_temperatures = np.linspace(1.0, reduced_intersect_temperatures[-1] + 0.1, 100)
-    polynomial_fit = np.poly1d(np.polyfit(reduced_intersect_temperatures, inverse_system_sizes, 1))
-    axes[1].plot(polynomial_fit(continuous_reduced_intersect_temperatures), continuous_reduced_intersect_temperatures,
-                 linestyle="--", color="black")
-    twinned_axis.plot(inverse_system_sizes, intersect_values, marker=".", markersize=8, color="red", linestyle="None")
+    inverse_log_squared_system_sizes = [1.0 / np.log(length ** 2) ** 2 for length in linear_system_sizes]
+    inverse_log_squared_system_sizes.reverse(), reduced_intersect_temperatures.reverse(), intersect_values.reverse()
+    inverse_log_squared_system_sizes.pop()
+    left_legend_1 = axes[1].plot(inverse_log_squared_system_sizes, reduced_intersect_temperatures, marker=".",
+                                 markersize=10, color="black", linestyle="None", label="intercept temps")
+    right_legend = twinned_axis.plot(inverse_log_squared_system_sizes, intersect_values, marker="*", markersize=10,
+                                     color="red", linestyle="-.", label="intercept values")
 
-    legend = axes[0].legend(loc="upper right", fontsize=8.11)
-    legend.get_frame().set_edgecolor("k"), legend.get_frame().set_lw(3)
+    """fit reduced intercept temperatures"""
+    continuous_inverse_log_squared_system_sizes = np.linspace(0.0, inverse_log_squared_system_sizes[-1] + 0.1, 100)
+    polyfit_params, polyfit_cov = np.polyfit(inverse_log_squared_system_sizes, reduced_intersect_temperatures, 1,
+                                             cov=True)
+    polyfit_errors = np.sqrt(np.diag(polyfit_cov))
+    print(f"Reduced intercept temperature = {polyfit_params[1]} +- {polyfit_errors[1]}")
+    polynomial_fit = np.poly1d(polyfit_params)
+    left_legend_2 = axes[1].plot(continuous_inverse_log_squared_system_sizes,
+                                 polynomial_fit(continuous_inverse_log_squared_system_sizes), linestyle="--",
+                                 color="black", label="temp. fit")
+
+    combined_legends_data = left_legend_1 + left_legend_2 + right_legend
+    combined_legends_labels = [legend.get_label() for legend in combined_legends_data]
+    legends = [axes[0].legend(loc="upper right", ncol=2, fontsize=7.625),
+               axes[1].legend(combined_legends_data, combined_legends_labels, loc=[0.625, 0.05], fontsize=8.5)]
+    [legend.get_frame().set_edgecolor("k") for legend in legends]
+    [legend.get_frame().set_lw(3.0) for legend in legends]
     fig.savefig(f"{output_directory}/magnetisation_phase_cramervonmises_xy_gaussian_noise_metropolis_and_ecmc.pdf",
                 bbox_inches="tight")
 
@@ -270,6 +287,10 @@ def compute_event_rates(algorithm_name, external_global_moves_string, output_dir
             event_rates_file.write(f"{temperature:.14e}".ljust(30) + f"{np.mean(event_rate_vs_job):.14e}".ljust(30) +
                                    f"{np.std(event_rate_vs_job) / len(event_rate_vs_job) ** 0.5:.14e}" + "\n")
         event_rates_file.close()
+
+
+def quadratic_fit(one_dimensional_array, a, b, c):
+    return a * one_dimensional_array ** 2 + b * one_dimensional_array + c
 
 
 if __name__ == "__main__":
