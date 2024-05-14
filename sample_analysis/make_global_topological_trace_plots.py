2737ee8c3b9c4186ec1d099511d12c08819824d9
michaelfaulkner <michael.faulkner@warwick.ac.uk>
2024-05-14 11:46:31 +0100

    make_global_topological_trace_plots.py: optimised presentation of trace plots

--- sample_analysis/make_global_topological_trace_plots.py
+++ sample_analysis/make_global_topological_trace_plots.py
@@ -55,41 +55,56 @@ def make_plots(sample_directories, output_directory, no_of_sites, no_of_sites_st
     """Define observables[i] as the observables of interest of models[i]..."""
     observables = [["electric_field_zero_mode", "topological_sector"],
                    ["macro_josephson_current", "hxy_topological_sector", "potential_minimising_twists"],
-                   ["xy_emergent_field_zero_mode", "xy_topological_sector", "xy_twist_relaxation_field",
-                    "potential_minimising_twists"]]
+                   ["xy_emergent_field_zero_mode", "xy_topological_sector", "potential_minimising_twists",
+                    "xy_twist_relaxation_field"]]
     """...then define the Cartesian coordinate of each observable (x = 0 / y = 1 for electro/spin-rep quantities)..."""
     observable_cartesian_coords = [[0, 0], [1, 0, 1], [0, 0, 1, 1]]
     """...then define the corresponding plot labels"""
-    plot_labels = [[r"$\widebar{E}_{\rm x} / (2\pi / \sqrt{N})$", r"$w_{\rm x}$"],
-                   [r"$\widebar{E}_{\rm x} / (2\pi / \sqrt{N})$", r"$w_{\rm x}$",
+    plot_labels = [[r"$\bar{E}_{\rm x} / (2\pi J / L)$", r"$w_{\rm x}$"],
+                   [r"$\bar{E}_{\rm x} / (2\pi J / L)$", r"$w_{\rm x}$",
                     r"$\tilde{t}_{\rm y}^{\rm non-anneal}$"],
-                   [r"$\widebar{E}_{\rm x} / (2\pi / \sqrt{N})$", r"$w_{\rm x}$", r"$\tilde{t}_{\rm y}$",
-                    r"$\tilde{t}_{\rm y}^{\rm non-anneal}$"]]
-    observable_plotting_colours = ["black", "blue", "red", "yellow"]
+                   [r"$\bar{E}_{\rm x} / (2\pi J / L)$", r"$w_{\rm x}$", r"$\tilde{t}_{\rm y}^{\rm non-anneal}$",
+                    r"$\tilde{t}_{\rm y}$"]]
+    observable_plotting_colours = ["black", "blue", "red", "orange"]
+    observable_marker_styles = [None, ".", "*", "D"]
     get_sample_methods = [[getattr(sample_getter, "get_" + observable_string)
                            for observable_string in model_observables] for model_observables in observables]
     run_indexed_sample_directories = [f"{sample_directory}/run_{run_index}" for sample_directory in sample_directories]
 
     for model_index, model in enumerate(models):
-        for temperature_index, temperature in enumerate(reduced_model_temperatures[model_index]):
+        for temp_index, temperature in enumerate(reduced_model_temperatures[model_index]):
             samples = np.array([get_sample_method(run_indexed_sample_directories[model_index], temperature,
-                                                  temperature_index, no_of_sites, no_of_equilibration_sweeps)
+                                                  temp_index, no_of_sites, no_of_equilibration_sweeps)
                                 for get_sample_method in get_sample_methods[model_index]])
-            # if model_index < 2:
             samples[0] = (no_of_sites ** 0.5) * samples[0] / 2.0 / math.pi
-            if temperature_index == 0:
-                [axes[temperature_index, model_index].plot(samples[observable_index, :no_of_observations,
+            if temp_index == 0:
+                for observable_index in reversed(range(len(samples))):
+                    if observable_index == 0:
+                        axes[temp_index, model_index].plot(samples[observable_index, :no_of_observations, 
                                                            observable_cartesian_coords[model_index][observable_index]],
-                                                           linestyle="solid",
+                                                           linestyle="-", linewidth=2.5, 
                                                            color=observable_plotting_colours[observable_index],
                                                            label=f"X = {plot_labels[model_index][observable_index]}")
-                 for observable_index in reversed(range(len(samples)))]
-            else:
-                [axes[temperature_index, model_index].plot(samples[observable_index, :no_of_observations,
+                    else:
+                        axes[temp_index, model_index].plot(samples[observable_index, :no_of_observations,
                                                            observable_cartesian_coords[model_index][observable_index]],
-                                                           linestyle="solid",
+                                                           marker=observable_marker_styles[observable_index],
+                                                           markersize=12, linestyle="-",  
+                                                           color=observable_plotting_colours[observable_index],
+                                                           label=f"X = {plot_labels[model_index][observable_index]}")
+            else:
+                for observable_index in reversed(range(len(samples))):
+                    if observable_index == 0:
+                        axes[temp_index, model_index].plot(samples[observable_index, :no_of_observations,
+                                                           observable_cartesian_coords[model_index][observable_index]],
+                                                           linestyle="-", linewidth=2.5,
                                                            color=observable_plotting_colours[observable_index])
-                 for observable_index in reversed(range(len(samples)))]
+                    else:
+                        axes[temp_index, model_index].plot(samples[observable_index, :no_of_observations,
+                                                           observable_cartesian_coords[model_index][observable_index]],
+                                                           marker=observable_marker_styles[observable_index],
+                                                           markersize=12, linestyle="-",
+                                                           color=observable_plotting_colours[observable_index])
     for model_index in range(len(models)):
         handles, labels = axes[0, model_index].get_legend_handles_labels()
         if model_index == 0:
@@ -102,19 +117,18 @@ def make_plots(sample_directories, output_directory, no_of_sites, no_of_sites_st
 
     inset_axis = plt.axes((0.8375, 0.545, 0.15, 0.15))
     inset_axis.tick_params(which='both', direction='in', length=5, width=3, labelsize=12, pad=2)
-    inset_axis.set_xlim([0.0, 4.999e3])
+    inset_axis.set_xlim([0.0, 2.499e3])
     inset_axis.xaxis.set_label_position("top")
     inset_axis.xaxis.tick_top()
     inset_axis.set_xlabel(r"$t / \Delta t_{\mathrm{Metrop}}$", fontsize=14, labelpad=3)
     inset_axis.set_ylim(-0.6, 0.6)
-    inset_axis.set_ylabel(
-        r"$\sum_{\langle \mathbf{r}, \mathbf{r}' \rangle_y} \sin (\varphi_\mathbf{r} - \varphi_{\mathbf{r}'}) / N$",
-        fontsize=12, labelpad=-8)
+    inset_axis.set_ylabel(r"$\sum_{\langle \mathbf{r}, \mathbf{r}' \rangle_y} "
+                          r"\sin (\varphi_\mathbf{r} - \varphi_{\mathbf{r}'}) / N$", fontsize=12, labelpad=-8)
     [inset_axis.spines[spine].set_linewidth(3) for spine in ["top", "bottom", "left", "right"]]
-    for temperature_index, temperature in reverse_enumerate(reduced_model_temperatures[2]):
+    for temp_index, temperature in reverse_enumerate(reduced_model_temperatures[2]):
         xy_macro_josephson_current_sample = get_macro_josephson_current(
-            run_indexed_sample_directories[2], temperature, temperature_index, no_of_sites, no_of_equilibration_sweeps)
-        if temperature_index == 0:
+            run_indexed_sample_directories[2], temperature, temp_index, no_of_sites, no_of_equilibration_sweeps)
+        if temp_index == 0:
             inset_axis.plot(xy_macro_josephson_current_sample[:no_of_observations, 1], linestyle="solid",
                             color="black", label=r"$\widetilde{\beta}_{\rm BKT}^{\rm XY} / \beta = 0.95$")
         else:
@@ -136,26 +150,26 @@ def setup_figure_axes(axes):
     [axis.tick_params(which='major', length=7, labelsize=22.5, pad=3) for axis in axes.flatten()]
     [axis.tick_params(which='minor', length=4) for axis in axes.flatten()]
     [axis.spines[spine].set_linewidth(3.75) for spine in ["top", "bottom", "left", "right"] for axis in axes.flatten()]
-    [axis.set_xlim([0.0, 5.0e3]) for axis in axes.flatten()]
+    [axis.set_xlim([0.0, 2.5e3]) for axis in axes.flatten()]
     [axis.set_ylim([-2.1, 2.1]) for axis in axes.flatten()]
     [axes[1, model_index].set_xlabel(r"$t / \Delta t_{\mathrm{Metrop}}$", fontsize=25) for model_index in range(3)]
     for model_index in range(3):
         if model_index == 0:
             for temp_index in range(2):
                 if temp_index == 0:
-                    axes[temp_index, model_index].set_ylabel(
-                        r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 0.95)$", fontsize=25, labelpad=-1.0)
+                    axes[temp_index, model_index].set_ylabel(r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 0.95)$",
+                                                             fontsize=25, labelpad=-1.0)
                 else:
-                    axes[temp_index, model_index].set_ylabel(
-                        r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 1.5)$", fontsize=25, labelpad=-1.0)
+                    axes[temp_index, model_index].set_ylabel(r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 1.5)$",
+                                                             fontsize=25, labelpad=-1.0)
         elif model_index == 1:
             for temp_index in range(2):
                 if temp_index == 0:
-                    axes[temp_index, model_index].set_ylabel(
-                        r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 0.95)$", fontsize=25, labelpad=-1.0)
+                    axes[temp_index, model_index].set_ylabel(r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 0.95)$",
+                                                             fontsize=25, labelpad=-1.0)
                 else:
-                    axes[temp_index, model_index].set_ylabel(
-                        r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 1.5)$", fontsize=25, labelpad=-1.0)
+                    axes[temp_index, model_index].set_ylabel(r"$X(t; \widetilde{\beta}_{\rm BKT} / \beta = 1.5)$",
+                                                             fontsize=25, labelpad=-1.0)
         else:
             for temp_index in range(2):
                 if temp_index == 0:
